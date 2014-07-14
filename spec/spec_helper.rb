dcda8244a8e525b99153afda77e799e0ad728c1a
John Kisor <jkisor@gmail.com>
2014-07-14 16:01:22 -0400

    extract member cv xml assertion into helper (to be reused)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -59,4 +59,5 @@ RSpec.configure do |config|
   config.extend ControllerMacros, :type => :controller
   config.include FactoryGirl::Syntax::Methods
   config.include Features, type: :feature
+  config.include Requests, type: :request
 end
