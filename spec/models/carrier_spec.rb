05af2f5e693700b88b8e81782a7c08a01cbe269e
John Kisor <jkisor@gmail.com>
2014-07-16 14:21:46 -0400

    carrier model specs

--- spec/models/carrier_spec.rb
+++ spec/models/carrier_spec.rb
@@ -1,5 +1,58 @@
 require 'spec_helper'
 
 describe Carrier do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:carrier) { build :carrier }
+  [
+    :name,
+    :abbrev,
+    :hbx_carrier_id,
+    :ind_hlt,
+    :ind_dtl,
+    :shp_hlt,
+    :shp_dtl,
+    :plans,
+    :policies,
+    :premium_payments,
+    :brokers,
+    :employers,
+    :carrier_profiles
+  ].each do |attribute|
+    it { should respond_to attribute }
+  end
+
+  it 'retrieves carriers in individual health market' do
+    carrier.ind_hlt = true;
+    carrier.save!
+    expect(Carrier.individual_market_health.to_a).to eq [carrier]
+  end
+
+  it 'retrieves carrier in individual dental market' do
+    carrier.ind_dtl = true;
+    carrier.save!
+    expect(Carrier.individual_market_dental.to_a).to eq [carrier]
+  end
+
+  it 'retrieves carrier in shop health market' do
+    carrier.shp_hlt = true;
+    carrier.save!
+    expect(Carrier.shop_market_health.to_a).to eq [carrier]
+  end
+
+  it 'retrieves carrier in shop dental market' do
+    carrier.shp_dtl = true;
+    carrier.save!
+    expect(Carrier.shop_market_dental.to_a).to eq [carrier]
+  end
+
+  it 'finds carrier by hbx id' do
+    carrier.save!
+    expect(Carrier.for_hbx_id(carrier.hbx_carrier_id)).to eq carrier
+  end
+
+  it 'find carriers by fein' do
+    fein = '1234'
+    carrier.carrier_profiles << CarrierProfile.new(fein: fein)
+    carrier.save!
+    expect(Carrier.for_fein(fein)).to eq carrier
+  end
 end
