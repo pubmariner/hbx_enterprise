f07ae83a8a85698e2c20b7f2c24f50d8af90e0db
Kevin Wei <kevin.tw.wei@gmail.com>
2014-07-28 14:06:40 -0400

    CV change vocabularies page now allows you to select which members are included on the CV

--- app/models/canonical_vocabulary/maintenance_serializer.rb
+++ app/models/canonical_vocabulary/maintenance_serializer.rb
@@ -13,20 +13,21 @@ module CanonicalVocabulary
       "xsi:schemaLocation" =>"http://dchealthlink.com/vocabularies/1/process process.xsd      http://dchealthlink.com/vocabularies/1/insured insured.xsd      http://dchealthlink.com/vocabularies/1/plan plan.xsd      http://dchealthlink.com/vocabularies/1/employer employer.xsd     http://dchealthlink.com/vocabularies/1/carrier carrier.xsd     http://dchealthlink.com/vocabularies/1/contact contacts.xsd     http://dchealthlink.com/vocabularies/1/base_types.xsd base_types.xsd"
     }
 
-    def initialize(policy, op, reas, m_ids, opts={})
+    def initialize(policy, op, reas, m_ids, include_m_ids, opts={})
       @options = opts
       @operation = op
       @policy = policy
       @reason = reas
       @member_ids = m_ids
+      @included_members_ids = include_m_ids
     end
 
     def serialize
-      en_ser = CanonicalVocabulary::EnrollmentSerializer.new(@policy, @options)
+      en_ser = CanonicalVocabulary::EnrollmentSerializer.new(@policy, @included_members_ids, @options)
       builder = Nokogiri::XML::Builder.new do |xml|
         xml['proc'].Operation(CV_XMLNS) do |xml|
           xml['proc'].operation do |xml|
-            xml['proc'].type(@operation) 
+            xml['proc'].type(@operation)
             xml['proc'].reason(@reason)
             xml['proc'].affected_members do |xml|
               @member_ids.each do |m|
