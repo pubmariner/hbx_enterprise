84f9b53acb368a2ddb96ae84ae51225c925ab31a
Trey Evans <lewis.r.evans@gmail.com>
2014-08-11 14:53:42 -0400

    Adding amqp config spec.

---
+++ spec/models/protocols/amqp/configuration_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe Protocols::Amqp::Configuration do
+  subject { Protocols::Amqp::Configuration }}
+
+  it "should have the connection url" do
+    expect(subject.connection_url).not_to be_nil
+  end
+
+end
