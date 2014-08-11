33b671661d65c1f08e15d2b14bd0de7c7bf65398
Trey Evans <lewis.r.evans@gmail.com>
2014-08-11 14:55:48 -0400

    Fixing syntax.

--- spec/models/protocols/amqp/configuration_spec.rb
+++ spec/models/protocols/amqp/configuration_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Protocols::Amqp::Configuration do
-  subject { Protocols::Amqp::Configuration }}
+  subject { Protocols::Amqp::Configuration }
 
   it "should have the connection url" do
     expect(subject.connection_url).not_to be_nil
