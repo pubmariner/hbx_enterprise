213a3d07fef416aca049f3344164f99baa0ed4b4
Trey Evans <lewis.r.evans@gmail.com>
2014-08-26 12:16:03 -0400

    Change member address spec skeleton.

---
+++ spec/use_cases/change_member_address_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe ChangeMemberAddress do
+
+  describe "should signal an error" do
+
+    it "when the member does not exist"
+
+    it "when the member has more than one active health policy"
+
+    it "when the member has more than one active dental policy"
+
+    it "when the member has no active policies"
+
+    it "when the member already has the provided address"
+
+  end
+
+end
