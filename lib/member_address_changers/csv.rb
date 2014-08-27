1973e9cd5e0e7f15d890fcea392b008d8990acb5
John Kisor <jkisor@gmail.com>
2014-08-27 16:30:00 -0400

    details is optional for change address error receiver

--- lib/member_address_changers/csv.rb
+++ lib/member_address_changers/csv.rb
@@ -6,7 +6,7 @@ module MemberAddressChangers
       @csv = out_csv
     end
 
-    def invalid_address(details)
+    def invalid_address(details = {})
       details.each_pair do |k, v|
         property_errors = Array(v)
         property_errors.each do |err|
@@ -15,19 +15,19 @@ module MemberAddressChangers
       end
     end
 
-    def no_such_member(details)
+    def no_such_member(details = {})
       @errors << "- Member #{details[:member_id]} does not exist\n"
     end
 
-    def too_many_health_policies(details)
+    def too_many_health_policies(details = {})
       @errors << "- has too many active health policies\n"
     end
 
-    def too_many_dental_policies(details)
+    def too_many_dental_policies(details = {})
       @errors << "- has too many active dental policies\n"
     end
 
-    def no_active_policies(details)
+    def no_active_policies(details = {})
       @errors << "- no active policies\n"
     end
 
