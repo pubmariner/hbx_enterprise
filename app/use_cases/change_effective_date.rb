9cbe4ce790d51c2ae598a52c1457f7eae478809b
John Kisor <jkisor@gmail.com>
2014-08-28 14:51:56 -0400

    change effective date use case complete

--- app/use_cases/change_effective_date.rb
+++ app/use_cases/change_effective_date.rb
@@ -1,6 +1,7 @@
 class ChangeEffectiveDate
-  def initialize(policy_repo)
+  def initialize(transmitter, policy_repo = Policy)
     @policy_repo = policy_repo
+    @transmitter = transmitter
   end
 
   def execute(request, listener)
@@ -12,23 +13,75 @@ class ChangeEffectiveDate
       return
     end
 
-    if(policy.enrollees.any? { |e| e.coverage_start == request[:effective_date] })
+    if(policy.subscriber.coverage_start == request[:effective_date])
       listener.no_changes_needed
       listener.fail
       return
     end
 
-    if(policy.enrollees.any? { |e| !e.coverage_end.blank? } )
+    if coverage_ended?(policy.subscriber)
       listener.policy_inactive
       listener.fail
       return
     end
 
-    policy.enrollees.each do |enrollee|
-      enrollee.coverage_start = request[:effective_date]
+    terminateds = terminated_enrollees(policy)
+    if terminateds.any?
+      listener.ambiguous_terminations(:member_ids => terminateds.map(&:m_id))
+      listener.fail
+      return
     end
-    
+
+    # policy.enrollees.each do |enrollee| 
+    #   if(enrollee.coverage_start != policy.subscriber.coverage_start)
+    #     listener.start_date_mismatch(coverage_start: [policy.subscriber.coverage_start, enrollee.coverage_start])
+    #     listener.fail
+    #     return
+    #   end
+    # end
+    coverage_starts = policy.enrollees.reject { |en| cancelled?(en) }.map(&:coverage_start)
+
+    if coverage_starts.uniq.length > 1
+      listener.start_date_mismatch(:coverage_start => coverage_starts)
+      listener.fail
+      return
+    end
+
+
+    affected_enrollees = []
+    policy.enrollees.each do |enrollee|
+      unless coverage_ended?(enrollee)
+        enrollee.coverage_start = request[:effective_date]
+        affected_enrollees << enrollee
+      end
+    end
+
+    transmit_request = {
+      policy_id: policy.id,
+      operation: 'change',
+      reason: 'benefit_selection',
+      affected_enrollee_ids: affected_enrollees.map(&:m_id), #todo
+      include_enrollee_ids: affected_enrollees.map(&:m_id),  #todo
+      current_user: request[:current_user]
+    }
+    @transmitter.execute(transmit_request)
     policy.save!
     listener.success
+  end
+
+  private
+
+  def cancelled?(enrollee)
+    enrollee.coverage_start == enrollee.coverage_end
+  end
+
+  def coverage_ended?(enrollee)
+    !enrollee.coverage_end.blank?
+  end
+
+  def terminated_enrollees(policy)
+    policy.enrollees.select do |e|
+      !e.coverage_end.blank? && (e.coverage_start != e.coverage_end)
+    end
   end
 end
--- spec/use_cases/change_effective_date_spec.rb
+++ spec/use_cases/change_effective_date_spec.rb
@@ -1,19 +1,35 @@
 require 'spec_helper'
 
 describe ChangeEffectiveDate do
-  subject(:change_effective_date) { ChangeEffectiveDate.new(policy_repo) }
+  subject(:change_effective_date) { ChangeEffectiveDate.new(transmitter, policy_repo) }
   let(:policy_repo) { double(find: policy) }
-  let(:policy) { double(enrollees: enrollees, save!: true)}
+  let(:policy) { double(id: '1234', enrollees: enrollees, save!: true, subscriber: subscriber)}
   let(:request) do
     {
       policy_id: '1234',
-      effective_date: Date.today.next_month
+      effective_date: Date.today.next_month,
+      current_user: current_user
     }
   end
 
-  let(:enrollees) { [ subscriber ] }
-  let(:subscriber) { Enrollee.new(coverage_start: Date.today) }
+  let(:enrollees) { [ subscriber, other_enrollee ] }
+  let(:other_enrollee) { Enrollee.new(coverage_start: Date.today, m_id: "4231") }
+  let(:subscriber) { Enrollee.new(coverage_start: Date.today, m_id: "5324", "rel_code" => "self") }
   let(:listener) { double(success: nil) }
+  let(:transmitter) { double(execute: nil ) }
+  let(:transmit_request) do
+    {
+      policy_id: policy.id,
+      operation: 'change',
+      reason: 'benefit_selection',
+      affected_enrollee_ids: affected_enrollee_ids,
+      include_enrollee_ids: affected_enrollee_ids,
+      current_user: current_user 
+    }
+  end
+  let(:affected_enrollee_ids) { [subscriber.m_id, other_enrollee.m_id] }
+
+  let(:current_user) { 'me@example.com' }
 
   it 'finds the policy' do
     expect(policy_repo).to receive(:find).with(request[:policy_id])
@@ -35,6 +51,11 @@ describe ChangeEffectiveDate do
     subject.execute(request, listener)
   end
 
+  it 'transmits the changes' do
+    expect(transmitter).to receive(:execute).with(transmit_request)
+    subject.execute(request, listener)
+  end
+
   context "when policy doesn't exist" do
     let(:policy_repo) { double(find: nil) }
     let(:listener) { double(no_such_policy: nil, fail: nil) }
@@ -51,7 +72,7 @@ describe ChangeEffectiveDate do
     end
   end
 
-  context 'when enrollee is canceled' do
+  context 'when subscriber is canceled' do
     let(:listener) { double(policy_inactive: nil, fail: nil)}
     let(:subscriber) { Enrollee.new(coverage_start: Date.today, coverage_end: Date.today) }
     
@@ -60,13 +81,13 @@ describe ChangeEffectiveDate do
       expect(listener).to receive(:fail)
       subject.execute(request, listener)
     end
-    it 'doesnt save the policy' do
+    it "doesn't save the policy" do
       expect(policy).not_to receive(:save!)
       subject.execute(request, listener)
     end
   end
   
-  context 'when enrollee is terminated' do
+  context 'when subscriber is terminated' do
     let(:listener) { double(policy_inactive: nil, fail: nil)}
     let(:subscriber) { Enrollee.new(coverage_start: Date.today, coverage_end: Date.today.next_month) }
 
@@ -81,12 +102,68 @@ describe ChangeEffectiveDate do
     end
   end
 
-  context 'when enrollee already has requested effective date' do
+  context 'when subscriber already has requested effective date' do
     let(:listener) { double(no_changes_needed: nil, fail: nil)}
     let(:subscriber) { Enrollee.new(coverage_start: request[:effective_date]) }
 
     it 'notifies the listener' do
       expect(listener).to receive(:no_changes_needed)
+      expect(listener).to receive(:fail)
+      subject.execute(request, listener)
+    end
+
+    it "doesn't save the policy" do
+      expect(policy).not_to receive(:save!)
+      subject.execute(request, listener)
+    end
+  end
+
+  context "when the other enrollee is cancelled" do
+    let(:other_enrollee) { Enrollee.new(coverage_start: Date.today, coverage_end: Date.today, m_id: "4231") }
+
+    let(:affected_enrollee_ids) { [subscriber.m_id] }
+    it 'changes only the subscriber' do
+
+      expect(policy).to receive(:save!)
+
+      subject.execute(request, listener)
+
+      expect(subscriber.coverage_start).to eq request[:effective_date]
+      expect(other_enrollee.coverage_start).not_to eq request[:effective_date]
+    end
+
+    it 'notifies listener' do
+      expect(listener).to receive(:success)
+      subject.execute(request, listener)
+    end
+
+    it 'transmits the changes' do
+      expect(transmitter).to receive(:execute).with(transmit_request)
+      subject.execute(request, listener)
+    end
+  end
+
+  context "when the other enrollee is terminated" do
+    let(:other_enrollee) { Enrollee.new(coverage_start: Date.today, coverage_end: Date.today.next_month, m_id: "4231") }
+    let(:listener) { double(ambiguous_terminations: nil, fail: nil) }
+    it 'notifies the listener' do
+      expect(listener).to receive(:ambiguous_terminations).with(:member_ids => [other_enrollee.m_id])
+      expect(listener).to receive(:fail)
+      subject.execute(request, listener)
+    end
+
+    it "doesn't save the policy" do
+      expect(policy).not_to receive(:save!)
+      subject.execute(request, listener)
+    end
+  end
+
+  context "when the other enrollee has a different coverage_start" do
+    let(:other_enrollee) { Enrollee.new(coverage_start: Date.today.next_month, m_id: "4231") }
+    let(:listener) { double(start_date_mismatch: nil, fail: nil) }
+
+    it 'notifies the listener' do
+      expect(listener).to receive(:start_date_mismatch).with(:coverage_start => [subscriber.coverage_start, other_enrollee.coverage_start])
       expect(listener).to receive(:fail)
       subject.execute(request, listener)
     end
