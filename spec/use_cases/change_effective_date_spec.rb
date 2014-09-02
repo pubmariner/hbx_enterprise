4d008049ddfcfee94392da535201e5aa31ca46f0
John Kisor <jkisor@gmail.com>
2014-09-02 16:01:46 -0400

    updating enrollee premium, policy premium total, and policy total responsible amount

--- spec/use_cases/change_effective_date_spec.rb
+++ spec/use_cases/change_effective_date_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe ChangeEffectiveDate do
   subject(:change_effective_date) { ChangeEffectiveDate.new(transmitter, policy_repo) }
   let(:policy_repo) { double(where: double(first: policy)) }
-  let(:policy) { double(id: '1234', enrollees: enrollees, save!: true, subscriber: subscriber)}
+  let(:policy) { double(id: '1234', enrollees: enrollees, save!: true, subscriber: subscriber, plan: plan, market: market, employer: employer, employer_contribution: 1.00, total_premium_amount: BigDecimal.new("100.0"), 'total_premium_amount=' => nil, 'employer_contribution=' => nil, 'total_responsible_amount=' => nil)}
+  let(:plan) { double(rate: looked_up_premium )}
+  let(:looked_up_premium) { double(amount: 123) }
+  let(:market) { 'individual' }
+  let(:employer) { nil }
   let(:request) do
     {
       policy_id: '1234',
@@ -13,8 +17,19 @@ describe ChangeEffectiveDate do
   end
 
   let(:enrollees) { [ subscriber, other_enrollee ] }
-  let(:other_enrollee) { Enrollee.new(coverage_start: Date.today, m_id: "4231") }
-  let(:subscriber) { Enrollee.new(coverage_start: Date.today, m_id: "5324", "rel_code" => "self") }
+  let(:other_enrollee) do
+    Enrollee.new(
+      coverage_start: Date.today, 
+      m_id: "4231"
+    )
+  end
+  let(:subscriber) do
+    Enrollee.new(
+      coverage_start: Date.today, 
+      m_id: "5324", 
+      rel_code: "self"
+    )
+  end
   let(:listener) { double(success: nil) }
   let(:transmitter) { double(execute: nil ) }
   let(:transmit_request) do
@@ -30,6 +45,12 @@ describe ChangeEffectiveDate do
   let(:affected_enrollee_ids) { [subscriber.m_id, other_enrollee.m_id] }
 
   let(:current_user) { 'me@example.com' }
+
+  before do
+    enrollees.each do |enrollee|
+      enrollee.stub(:member) { double(dob: Date.new(1980,02,01)) }
+    end
+  end
 
   it 'finds the policy' do
     expect(policy_repo).to receive(:where).with({"_id" => request[:policy_id]})
@@ -55,6 +76,42 @@ describe ChangeEffectiveDate do
     expect(transmitter).to receive(:execute).with(transmit_request)
     subject.execute(request, listener)
   end
+
+  context 'when policy is for individual market' do 
+    let(:market) { 'individual' }
+    it "updates enrollees' premium based on the new effective_date" do
+      enrollees.each do |enrollee|
+        new_effective_date = Date.parse(request[:effective_date])
+        expect(policy.plan).to receive(:rate).with(new_effective_date, new_effective_date, enrollee.member.dob)
+      end
+
+      subject.execute(request, listener)
+        
+      enrollees.each do |enrollee|
+        expect(enrollee.pre_amt).to eq(looked_up_premium.amount)
+      end
+    end
+  end
+
+  context 'when policy is for shop market' do
+    let(:market) { 'shop' }
+    let(:employer) { double(plan_year_start: Date.today.prev_year.prev_week)}
+
+    it "updates enrollees' premium based on the new effective_date and employer plan year start" do
+      enrollees.each do |enrollee|
+        new_effective_date = Date.parse(request[:effective_date])
+        expect(policy.plan).to receive(:rate).with(employer.plan_year_start, new_effective_date, enrollee.member.dob)
+      end
+
+      subject.execute(request, listener)
+        
+      enrollees.each do |enrollee|
+        expect(enrollee.pre_amt).to eq(looked_up_premium.amount)
+      end
+    end
+
+  end
+
 
   context "when policy doesn't exist" do
     let(:policy_repo) { double(where: double(first: nil)) }
