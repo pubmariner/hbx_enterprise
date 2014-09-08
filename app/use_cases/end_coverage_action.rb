98833e10a5b6261e79a60ba53bc3090e2d175026
John Kisor <jkisor@gmail.com>
2014-09-08 15:02:24 -0400

    Extract Null Policy Maintenance class

--- app/use_cases/end_coverage_action.rb
+++ app/use_cases/end_coverage_action.rb
@@ -1,14 +1,9 @@
-class NullPolicyMaintenanceAction
-  def execute(request)
-  end
-end
-
 class EndCoverageAction
   def self.create_for(request)
     if(request[:transmit])
       TransmitPolicyMaintenance.new
     else
-      NullPolicyMaintenanceAction.new
+      NullPolicyMaintenance.new
     end
   end
 end
