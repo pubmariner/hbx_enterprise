3a4575d39c3cc73b6f5ba0316a157210dec79be7
Kevin Wei <kevin.tw.wei@gmail.com>
2014-09-09 09:33:52 -0400

    update steven all script

--- script/queries/steven_all.rb
+++ script/queries/steven_all.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-policies = Policy.where(
+policies = Policy.no_timeout.where(
   {"eg_id" => {"$not" => /DC0.{32}/}}
 )
 
@@ -13,7 +13,7 @@ CSV.open("steven_all.csv", 'w') do |csv|
   policies.each do |pol|
     if !bad_eg_id(pol.eg_id)
       if !pol.subscriber.nil?
-        if !pol.subscriber.cancelled?
+        if !pol.subscriber.canceled?
           pol.enrollees.each do |en|
             per = en.person
             csv << [pol.eg_id, pol.plan.hios_plan_id, (pol.employer_id.nil? ? "QHP" : "SHOP"), pol.plan.name, pol.carrier.name, en.m_id, per.name_first, per.name_middle, per.name_last, en.member.dob.strftime("%Y%m%d"), en.member.ssn, (per.emails.first.nil? ? "" : per.emails.first.email_address), (per.phones.first.nil? ? "" : per.phones.first.phone_number), pol.employer_id.blank? ? "N" : "Y"]
