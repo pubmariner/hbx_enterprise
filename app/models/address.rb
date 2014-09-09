50004a6f9ea7b715357c033a7e56f16e3ff5d8c8
John Kisor <jkisor@gmail.com>
2014-09-09 14:32:16 -0400

    better zip code validation.

--- app/models/address.rb
+++ app/models/address.rb
@@ -18,7 +18,7 @@ class Address
 
   validates_presence_of :address_1, :city, :state, :zip
 
-  validates_length_of :zip, :minimum => 5, :allow_blank => false, :allow_nil => false
+  validates_format_of :zip, :with => /^\d{5}(-\d{4})?$/, :message => "should be in the form 12345 or 12345-1234"
 
   embedded_in :person, :inverse_of => :addresses
   embedded_in :employer, :inverse_of => :addresses
