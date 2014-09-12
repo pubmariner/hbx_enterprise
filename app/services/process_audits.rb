e9ef080e7add402dd667b55e3bcdbba7a153ebbc
Trey Evans <lewis.r.evans@gmail.com>
2014-09-12 16:46:03 -0400

    Still fixing audits.

--- app/services/process_audits.rb
+++ app/services/process_audits.rb
@@ -2,40 +2,27 @@ class ProcessAudits
   def self.execute(active_start, active_end, term_start, term_end, other_params, out_directory)
     active_audits = Policy.find_active_and_unterminated_in_range(active_start, active_end, other_params).no_timeout
     term_audits = Policy.find_terminated_in_range(term_start, term_end, other_params).no_timeout
-    active_audits.each do |term|
+
+    audit_ids = active_audits.map(&:id) | term_audits.map(&:id)
+
+    audits = Policy.where("id" => {"$in" => audit_ids})
+    audits.each do |term|
       # TODO: Make the list of included ids match non-cancelled,
       # non-termed as of X date members
       enrollee_list = term.enrollees.reject { |en| en.canceled? }
-      enrollee_list = enrollee_list.select do |en|
-        en.coverage_end.nil? || (en.coverage_end > active_end)
+      subscriber = term.subscriber.m_id
+      enrollee_list = enrollee_list.reject do |en|
+        !en.coverage_end.blank? && (en.coverage_end < term_start) 
       end
-      out_f = File.open(File.join(out_directory, "#{term._id}_active.xml"), 'w')
+      all_ids = enrollee_list.map(&:m_id) | [subscriber]
+      out_f = File.open(File.join(out_directory, "#{term._id}_audit.xml"), 'w')
       ser = CanonicalVocabulary::MaintenanceSerializer.new(
         term,
         "audit",
         "notification_only",
-        enrollee_list.map(&:m_id),
-        enrollee_list.map(&:m_id),
+        all_ids,
+        all_ids,
         { :term_boundry => active_end }
-      )
-      out_f.write(ser.serialize)
-      out_f.close
-    end
-    term_audits.each do |term|
-      # TODO: Make the list of included ids match non-cancelled,
-      # termed as of X date members
-      enrollee_list = term.enrollees.reject { |en| en.canceled? }
-      enrollee_list = enrollee_list.select do |en|
-        !en.coverage_end.nil? && (en.coverage_end <= active_end)
-      end
-      out_f = File.open(File.join(out_directory, "#{term._id}_term.xml"), 'w')
-      ser = CanonicalVocabulary::MaintenanceSerializer.new(
-        term,
-        "audit",
-        "notification_only",
-        enrollee_list.map(&:m_id),
-        enrollee_list.map(&:m_id),
-        { :term_boundry => term_end }
       )
       out_f.write(ser.serialize)
       out_f.close
