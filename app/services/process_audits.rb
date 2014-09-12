5c2868e9b74e3818d8adb4e410e7c156b0270cfb
Trey Evans <lewis.r.evans@gmail.com>
2014-09-12 16:13:08 -0400

    Fixing errors.

--- app/services/process_audits.rb
+++ app/services/process_audits.rb
@@ -1,11 +1,11 @@
 class ProcessAudits
   def self.execute(active_start, active_end, term_start, term_end, other_params, out_directory)
-    active_audits = Policy.find_active_and_unterminated_in_range(active_start, active_end, other_params)
-    term_audits = Policy.find_terminated_in_range(term_start, term_end, other_params)
+    active_audits = Policy.find_active_and_unterminated_in_range(active_start, active_end, other_params).no_timeout
+    term_audits = Policy.find_terminated_in_range(term_start, term_end, other_params).no_timeout
     active_audits.each do |term|
       # TODO: Make the list of included ids match non-cancelled,
       # non-termed as of X date members
-      enrollee_list = term.enrollees.reject { |en| en.cancelled? }
+      enrollee_list = term.enrollees.reject { |en| en.canceled? }
       enrollee_list = enrollee_list.select do |en|
         en.coverage_end.nil? || (en.coverage_end > active_end)
       end
@@ -15,6 +15,7 @@ class ProcessAudits
         "audit",
         "notification_only",
         enrollee_list.map(&:m_id),
+        enrollee_list.map(&:m_id),
         { :term_boundry => active_end }
       )
       out_f.write(ser.serialize)
@@ -23,7 +24,7 @@ class ProcessAudits
     term_audits.each do |term|
       # TODO: Make the list of included ids match non-cancelled,
       # termed as of X date members
-      enrollee_list = term.enrollees.reject { |en| en.cancelled? }
+      enrollee_list = term.enrollees.reject { |en| en.canceled? }
       enrollee_list = enrollee_list.select do |en|
         !en.coverage_end.nil? && (en.coverage_end <= active_end)
       end
@@ -32,6 +33,7 @@ class ProcessAudits
         term,
         "audit",
         "notification_only",
+        enrollee_list.map(&:m_id),
         enrollee_list.map(&:m_id),
         { :term_boundry => term_end }
       )
