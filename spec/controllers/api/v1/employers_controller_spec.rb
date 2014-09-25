3dbee59552c36bfe777dcf04661a36b505ea2e66
Trey Evans <lewis.r.evans@gmail.com>
2014-09-25 16:03:46 -0400

    Fixing specs.

--- spec/controllers/api/v1/employers_controller_spec.rb
+++ spec/controllers/api/v1/employers_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Api::V1::EmployersController, :type => :controller do
   login_user
--- spec/features/vocab_upload/uploading_individual_cv_spec.rb
+++ spec/features/vocab_upload/uploading_individual_cv_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 feature 'uploading individual CV' do
   given(:premium) do
