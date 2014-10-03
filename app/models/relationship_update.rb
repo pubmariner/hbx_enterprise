6715a7e0d63819162c6a8bbcc7e0ab5736d2349f
Trey Evans <lewis.r.evans@gmail.com>
2014-10-03 08:54:56 -0400

    Adding relationship update.

---
+++ app/models/relationship_update.rb
@@ -0,0 +1,36 @@
+class RelationshipUpdate
+  attr_accessor :subject_person_id, :relationship_kind, :object_person_id
+  KEYS = [:subject_person_id, :relationship_kind, :object_person_id]
+
+  def initialize(data = {})
+    data.each_pair do |k, v|
+      if KEYS.include?(k.to_sym)
+        self.send("#{k}=", v)
+      end
+    end
+  end
+
+  def subject_person
+    Person.find_by_id(@subject_person_id)
+  end
+
+  def object_person
+    Person.find_by_id(@object_person_id)
+  end
+
+  def save
+    new_rel = PersonRelationship.new({
+      :subject_person => subject_person,
+      :object_person => object_person,
+      :relationship_kind => relationship_kind
+    })
+    old_rels = subject_person.person_relationships.select do |rel|
+      rel.object_person_id == object_person.id
+    end
+    old_rels.each do |old_rel|
+      subject_person.person_relationships.delete(old_rel)
+    end
+    subject_person.person_relationships << new_rel
+    subject_person.save!
+  end
+end
