b2c8ed479c8f86c6d5425d1e4a484560340c5a74
Kevin Wei <kevin.tw.wei@gmail.com>
2014-10-03 16:38:45 -0400

    aliased income and phone model fields for XSD standard

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -8,7 +8,7 @@ class Phone
   field :phone_type, type: String
   field :phone_number, type: String
   field :extension, type: String
-  field :primary, type: Boolean
+  field :primary, as: :preferred, type: Boolean
 
   validates_presence_of  :phone_number
   validates_presence_of  :phone_type, message: "Choose a type"
