8cc64df084c820bf2654848a105bf603b8c7fb80
Trey Evans <lewis.r.evans@gmail.com>
2014-10-03 20:10:29 -0400

    Queries.

--- script/queries/set_household_policies.rb
+++ script/queries/set_household_policies.rb
@@ -1,12 +1,8 @@
 active_pols = Policy.where(Policy.active_as_of_expression(Date.new(2014, 12, 31)).merge({"employer_id" => nil}))
 
-all_subscribers = (active_pols.map do |pol|
-    pol.subscriber.person.id
-end).uniq
-
-all_subscribers.each do |sub|
-  count = ApplicationGroup.where(
+active_pols.map do |pol|
+  subscriber_person = pol.subscriber.person.id
+  ags = ApplicationGroup.where(
     :people_ids => { "$in" => all_subscribers }   
   )
-  puts count.count.to_s
 end
