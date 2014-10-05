ac8a2bc8d611e427319ed1994c049d12b2ee1109
Trey Evans <lewis.r.evans@gmail.com>
2014-10-05 19:21:12 -0400

    script/queries/generate_active_policy_application_groups.rb

---
+++ script/queries/generate_active_policy_application_groups.rb
@@ -0,0 +1,42 @@
+require 'securerandom'
+
+active_pols = Policy.where(Policy.active_as_of_expression(Date.new(2014, 12, 31)).merge({"employer_id" => nil, "responsible_party_id" => nil,
+"applied_aptc" => {"$in" => ["0", "0.0", "0.00"]}
+}))
+
+def create_me_a_group(sub, people)
+  ag = ApplicationGroup.new(
+    :e_case_id => SecureRandom.uuid.gsub("-", ""),
+    :primary_applicant_id => sub,
+    :person_ids => people
+  )
+  ag
+end
+
+def add_people_to_group(group, people_ids)
+  group.person_ids  = group.person_ids & people_ids
+  group.save!
+end
+
+
+active_pols.each do |pol|
+  subscriber_person = pol.subscriber.person.id
+  member_people = pol.enrollees.map { |en| en.person.id }
+  ags = ApplicationGroup.where(
+    :person_ids => {
+      "$elemMatch" => { "$in" => [subscriber_person]}
+    }
+  )
+  case ags.count
+  when 0
+    application_group = create_me_a_group(sub, people)
+    pol.application_group = ags.first
+    pol.save!
+  when 1
+    ag = ags.first
+    add_people_to_group(ag, member_people)
+    pol.application_group = ag
+    pol.save
+  else
+  end
+end
