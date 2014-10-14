36d38f912f0f3318356d2061003d90be90b4ee36
Trey Evans <lewis.r.evans@gmail.com>
2014-10-14 16:55:32 -0400

    Cleanup.

--- app/models/protocols/csv/csv_transmission.rb
+++ app/models/protocols/csv/csv_transmission.rb
@@ -2,14 +2,11 @@ module Protocols::Csv
   class CsvTransmission
     include Mongoid::Document
 
-    # TODO: Accept this in request
     field :batch_id, type: String
-    # TODO: Accept this in request
     field :file_name, type: String
-    # TODO: Accept this in request
     field :submitted_by, type: String
 
-    belongs_to :carrier
+    belongs_to :carrier, :index => true
 
     has_many :csv_transactions, :class_name => "Protocols::Csv::CsvTransaction"
 
