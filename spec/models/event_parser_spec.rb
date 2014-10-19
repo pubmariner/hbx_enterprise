4a197e8412681038e4c59a62275f98d89c7ec9f9
Trey Evans <lewis.r.evans@gmail.com>
2014-10-19 14:34:19 -0400

    Finished most model work.

--- spec/models/event_parser_spec.rb
+++ spec/models/event_parser_spec.rb
@@ -16,7 +16,7 @@ describe EventParser do
       Nokogiri::XML(event_file)
     }
     let(:event_uri) { "urn:openhbx:events:v1:employers_employees#qhp_selected" }
-    let(:message_headers) { {:headers=>{"event_name"=>"urn:openhbx:events:v1:employers_employees#qhp_selected", "qualifying_reason"=>"", "employer_uri"=>"", "enrollment_group_uri"=>"", "hbx_id"=>"DC0", "submitted_timestamp"=>"2014-03-07T12:00:00Z", "authorization"=>"", "originating_service"=>"orig_serv"}, "message_id"=>"msg_id"} }
+    let(:message_headers) { {:headers=>{"event_name"=>"urn:openhbx:events:v1:employers_employees#qhp_selected", "qualifying_reason"=>"", "employer_uri"=>"", "enrollment_group_uri"=>"", "hbx_id"=>"DC0", "submitted_timestamp"=>"2014-03-07T12:00:00Z", "authorization"=>"usr_token", "originating_service"=>"orig_serv"}, "message_id"=>"msg_id"} }
 
     it "should parse the correct event_uri" do
       expect(subject.event_uri).to eq event_uri
