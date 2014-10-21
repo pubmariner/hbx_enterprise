a399e5e02c40707789d39d680e6bc07b8f3ecde9
Trey Evans <lewis.r.evans@gmail.com>
2014-10-21 11:15:50 -0400

    Began refactoring.

--- app/controllers/soap/individuals_controller.rb
+++ app/controllers/soap/individuals_controller.rb
@@ -1,12 +1,5 @@
-class Soap::IndividualsController < ApplicationController
-  skip_before_filter :authenticate_user_from_token!
-  skip_before_filter :authenticate_me!
-  protect_from_forgery :except => [:get_by_hbx_id, :wsdl]
-
-  SOAP_NS = {
-    :soap => 'http://www.w3.org/2001/12/soap-envelope',
-    :cv_soap => 'http://openhbx.org/api/transports/soap/1.0'
-  }
+class Soap::IndividualsController < Soap::SoapController
+  protect_from_forgery :except => [:get_by_hbx_id]
 
   def get_by_hbx_id
     in_req = get_soap_body
@@ -21,34 +14,13 @@ class Soap::IndividualsController < ApplicationController
       return
     end
     @people = Person.where('members.hbx_member_id' => hbx_id)
-    render 'index', :content_type => "text/xml"
+    render 'get_by_hbx_id', :content_type => "text/xml"
   end
 
-  def wsdl
-    render action: 'wsdl', :content_type => "text/xml", :formats => [:xml]
-  end
-
-  def fail_authentication(user_token)
-    user = user_token && User.find_by_authentication_token(user_token.to_s)
-
-    if user
-      sign_in user, store: false
-      return false
-    end
-    true
-  end
-
-  def get_user_token(xml_body)
-    node = (xml_body.xpath("//user_token", SOAP_NS) | xml_body.xpath("//cv_soap:user_token", SOAP_NS)) .first
-    MayBlank.new(node).text.value
-  end
 
   def get_hbx_id(xml_body)
     node = (xml_body.xpath("//hbx_id", SOAP_NS) | xml_body.xpath("//cv_soap:hbx_id", SOAP_NS)).first
     MayBlank.new(node).text.value
   end
 
-  def get_soap_body
-    Nokogiri::XML(request.body.read)
-  end
 end
