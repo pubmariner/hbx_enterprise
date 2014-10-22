238d7f366190abecf911977ce69a34175f81fd4b
Trey Evans <lewis.r.evans@gmail.com>
2014-10-22 15:47:56 -0400

    Correcting unicorn config.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
-root_path = File.expand_path("..", File.dirname(__FILE__))
-shared_path = File.expand_path(root_path, "..", "shared")
+root_path = File.expand_path(File.join("..", File.dirname(__FILE__)))
+shared_path = File.expand_path(File.join(root_path, "..", "shared"))
 
 working_directory root_path
 pid shared_path + "/pids/unicorn.pid"
