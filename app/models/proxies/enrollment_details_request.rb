afd0f3c643da8dc17fcc0f6ff0d073d172a50a7c
Trey Evans <lewis.r.evans@gmail.com>
2014-10-22 10:47:46 -0400

    Extracting common code.

--- app/models/proxies/enrollment_details_request.rb
+++ app/models/proxies/enrollment_details_request.rb
@@ -1,16 +1,5 @@
 module Proxies
-  class EnrollmentDetailsRequest 
-    def self.request(en_id)
-      self.new.request(en_id)
-    end
-
-    def request(en_id)
-      uri = URI.parse(endpoint)
-      req = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'text/xml'})
-      req.body = template(en_id)
-      Net::HTTP.new(uri.host, uri.port).request(req).body
-    end
-
+  class EnrollmentDetailsRequest < Proxies::SimpleSoapRequest
     def template(en_id)
       soap_template = <<-XMLCODE
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
@@ -24,12 +13,8 @@ XMLCODE
       soap_template
     end
 
-    def endpoint
-      osb_host + "/soa-infra/services/COTS/ESB_GetInsurancePlanDetailsDService/GetInsurancePlanDetails_Mediator_ep"
-    end
-
-    def osb_host
-      ExchangeInformation.osb_host
+    def service_location 
+      "/soa-infra/services/COTS/ESB_GetInsurancePlanDetailsDService/GetInsurancePlanDetails_Mediator_ep"
     end
   end
 end
