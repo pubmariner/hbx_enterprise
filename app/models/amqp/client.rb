f204372dc466709157f72de69dd9359131027574
Trey Evans <lewis.r.evans@gmail.com>
2014-10-24 14:03:03 -0400

    Adding new stuff.

--- app/models/amqp/client.rb
+++ app/models/amqp/client.rb
@@ -12,10 +12,31 @@ module Amqp
         begin
           on_message(delivery_info, properties, payload)
         rescue => e
+          $stderr.puts "=== Processing Failure ==="
           $stderr.puts e.message
           $stderr.puts e.inspect
           $stderr.puts e.backtrace.join("\n")
-          throw :terminate, e
+          begin
+            existing_retry_count = properties.headers["x-redelivery-count"].to_i
+            if existing_retry_count > 5
+              $stderr.puts "=== Redelivery Attempts Exceeded ==="
+              $stderr.puts properties.to_hash.inspect
+              $stderr.puts payload
+            else
+              new_properties = properties.to_hash.dup
+              new_headers = new_properties[:headers] || {}
+              new_headers["x-redelivery-count"] = existing_retry_count + 1
+              new_properties[:headers] = new_headers
+              queue.publish(payload, new_properties)
+            end
+            channel.acknowledge(delivery_info.delivery_tag, false)
+          rescue => e
+            $stderr.puts "=== Redelivery Failure ==="
+            $stderr.puts e.message
+            $stderr.puts e.inspect
+            $stderr.puts e.backtrace.join("\n")
+            throw :terminate, e
+          end
         end
       end
     end
