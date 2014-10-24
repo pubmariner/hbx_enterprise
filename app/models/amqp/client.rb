e2a7e8b37cc11e0e59236e57ed07a4704da359bb
Trey Evans <lewis.r.evans@gmail.com>
2014-10-24 14:08:33 -0400

    Cleaning up client.

--- app/models/amqp/client.rb
+++ app/models/amqp/client.rb
@@ -7,15 +7,19 @@ module Amqp
       @queue = q
     end
 
+    def fail_with(err)
+      $stderr.puts err.message
+      $stderr.puts err.inspect
+      $stderr.puts err.backtrace.join("\n")
+    end
+
     def subscribe(opts = {})
       @queue.subscribe(opts) do |delivery_info, properties, payload|
         begin
           on_message(delivery_info, properties, payload)
         rescue => e
           $stderr.puts "=== Processing Failure ==="
-          $stderr.puts e.message
-          $stderr.puts e.inspect
-          $stderr.puts e.backtrace.join("\n")
+          fail_with(e)
           begin
             existing_retry_count = properties.headers["x-redelivery-count"].to_i
             if existing_retry_count > 5
@@ -23,22 +27,25 @@ module Amqp
               $stderr.puts properties.to_hash.inspect
               $stderr.puts payload
             else
-              new_properties = properties.to_hash.dup
-              new_headers = new_properties[:headers] || {}
-              new_headers["x-redelivery-count"] = existing_retry_count + 1
-              new_properties[:headers] = new_headers
+              new_properties = redelivery_properties(existing_retry_count, properties)
               queue.publish(payload, new_properties)
             end
             channel.acknowledge(delivery_info.delivery_tag, false)
           rescue => e
             $stderr.puts "=== Redelivery Failure ==="
-            $stderr.puts e.message
-            $stderr.puts e.inspect
-            $stderr.puts e.backtrace.join("\n")
+            fail_with(e)
             throw :terminate, e
           end
         end
       end
+    end
+
+    def redelivery_properties(existing_retry_count, properties)
+      new_properties = properties.to_hash.dup
+      new_headers = new_properties[:headers] || {}
+      new_headers["x-redelivery-count"] = existing_retry_count + 1
+      new_properties[:headers] = new_headers
+      new_properties
     end
   end
 end
