2b614531ed53dedd9a13709f79c816fb0e440b24
Trey Evans <lewis.r.evans@gmail.com>
2014-11-10 13:55:59 -0500

    Ready to begin edi_ops queue.

---
+++ app/amqp/edi_ops_listener.rb
@@ -0,0 +1,1 @@
+EdiQueueListener.run
--- app/models/listeners/edi_queue_listener.rb
+++ app/models/listeners/edi_queue_listener.rb
@@ -4,5 +4,14 @@ module Listeners
       ec = ExchangeInformation
       "#{ec.hbx_id}.#{ec.environment}.q.glue.edi_ops"
     end
+
+    def self.run
+      conn = Bunny.new(ExchangeInformation.amqp_uri)
+      conn.start
+      chan = conn.create_channel
+      chan.prefetch(1)
+      q = chan.queue(self.queue_name, :durable => true)
+      self.new(chan, q).subscribe(:block => true, :manual_ack => true)
+    end
   end
 end
