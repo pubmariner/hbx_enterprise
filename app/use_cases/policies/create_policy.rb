886da67d5e948c2dea000296938d16ed08169c81
Trey Evans <lewis.r.evans@gmail.com>
2014-11-12 15:31:19 -0500

    Policy create use case.

--- app/use_cases/policies/create_policy.rb
+++ app/use_cases/policies/create_policy.rb
@@ -1,23 +1,23 @@
 module Policies
   class CreatePolicy
 
+    def initialize(policy_factory = Policy)
+      @policy_factory = policy_factory
+    end
+
     def validate(request, listener)
       failed = false
       eg_id = request[:enrollment_group_id]
       hios_id = request[:hios_id]
       plan_year = request[:plan_year]
       broker_npn = request[:broker_npn]
-      existing_policy = Policy.find_for_group_and_hios(eg_id, hios_id)
+      enrollees = request[:enrollees]
+      existing_policy = @policy_factory.find_for_group_and_hios(eg_id, hios_id)
       if !existing_policy.blank?
         listener.policy_already_exists({
           :enrollment_group_id => eg_id,
           :hios_id => hios_id
         })
-        fail = true
-      end
-      plan = Plan.find_by_hios_id_and_year(hios_id, plan_year)
-      if plan.blank?
-        listener.plan_not_found(:hios_id => hios_id, :plan_year => plan_year)
         fail = true
       end
       if !broker_npn.blank?
@@ -27,7 +27,39 @@ module Policies
           fail = true
         end
       end
+      plan = Plan.find_by_hios_id_and_year(hios_id, plan_year)
+      if plan.blank?
+        listener.plan_not_found(:hios_id => hios_id, :plan_year => plan_year)
+        return false
+      end
+      policy = @policy_factory.new(request.merge({:plan => plan, :carrier => plan.carrier}))
+      if !policy.valid?
+        listener.invalid_policy(policy.errors.to_hash)
+        fail = true
+      end
+      if enrollees.blank?
+        listener.no_enrollees
+        fail = true
+      end
       !fail
+    end
+
+    def execute(request)
+      hios_id = request[:hios_id]
+      plan_year = request[:plan_year]
+      broker_npn = request[:broker_npn]
+
+      plan = Plan.find_by_hios_id_and_year(hios_id, plan_year)
+      broker = nil
+      if !broker_npn.blank?
+        broker = Broker.find_by_npn(broker_npn)
+      end
+
+      @policy_factory.create!(request.merge({
+        :plan => plan,
+        :carrier => plan.carrier,
+        :broker => broker
+      }))
     end
   end
 end
--- spec/use_cases/policies/create_policy_spec.rb
+++ spec/use_cases/policies/create_policy_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 describe Policies::CreatePolicy do
   let(:listener) { double(:fail => nil, :policy_already_exists => nil, :plan_not_found => nil) }
-  let(:request) { {:enrollment_group_id => enrollment_group_id, :hios_id => hios_id, :plan_year => plan_year} }
+  let(:request) { { :enrollment_group_id => enrollment_group_id, :hios_id => hios_id, :plan_year => plan_year, :enrollees => enrollees } }
   let(:hios_id) { "DLJKFKLSDJEF" }
   let(:enrollment_group_id) { "LSJKDKLFJEF" }
   let(:existing_policy) { nil }
   let(:plan_year) { "2015" }
-  let(:plan) { double }
+  let(:carrier) { double }
+  let(:plan) { double(:carrier => carrier) }
+  let(:subscriber) { double }
+  let(:enrollees) { [subscriber] }
+  let(:policy_factory) { double(:new => new_policy) }
+  let(:new_policy) { double(:valid? => valid_policy, :errors => policy_errors) }
+  let(:valid_policy) { true }
+  let(:policy_errors) { { "an error" => "a reason" } }
 
-  subject { Policies::CreatePolicy.new }
+  subject { Policies::CreatePolicy.new(policy_factory) }
 
   before :each do
-    allow(Policy).to receive(:find_for_group_and_hios).with(
+    allow(policy_factory).to receive(:find_for_group_and_hios).with(
       enrollment_group_id,
       hios_id
     ).and_return(existing_policy)
@@ -20,6 +27,19 @@ describe Policies::CreatePolicy do
       hios_id,
       plan_year
     ).and_return(plan)
+  end
+
+  it "should validate" do
+    expect(subject.validate(request, listener)).to be_truthy
+  end
+
+  describe "with an invalid policy" do
+    let(:valid_policy) { false }
+
+    it "should notify the listener of failure" do
+      expect(listener).to receive(:invalid_policy).with(policy_errors)
+      expect(subject.validate(request, listener)).to be_falsey
+    end
   end
 
   describe "with an already existing policy" do
@@ -48,7 +68,7 @@ describe Policies::CreatePolicy do
 
   describe "with a broker that doesn't exist" do
     let(:npn) { "andskflnsdf" }
-    let(:request) { {:enrollment_group_id => enrollment_group_id, :hios_id => hios_id, :plan_year => plan_year, :broker_npn => npn} }
+    let(:broker_request) { request.merge({:broker_npn => npn}) }
 
     before(:each) do
       allow(Broker).to receive(:find_by_npn).with(npn).and_return(nil)
@@ -56,7 +76,47 @@ describe Policies::CreatePolicy do
 
     it "should notify the listener of failure" do
       expect(listener).to receive(:broker_not_found).with({:npn => npn})
+      expect(subject.validate(broker_request, listener)).to be_falsey
+    end
+  end
+
+  describe "with no enrollees" do
+    let(:enrollees) { [] }
+
+    it "should notify the listener of failure" do
+      expect(listener).to receive(:no_enrollees)
       expect(subject.validate(request, listener)).to be_falsey
+    end
+  end
+
+  describe "persisting a policy" do
+    let(:create_params) {
+      request.merge({
+        :plan => plan,
+        :carrier => carrier,
+        :broker => nil
+      })
+    }
+
+    it "should create the policy" do
+      expect(policy_factory).to receive(:create!).with(create_params)
+      subject.execute(request)
+    end
+
+    describe "with a broker" do
+      let(:npn) { "andskflnsdf" }
+      let(:broker_create_params) { create_params.merge(:broker => broker, :broker_npn => npn) }
+      let(:broker_request) { request.merge(:broker_npn => npn) }
+      let(:broker) { double }
+
+      before(:each) do
+        allow(Broker).to receive(:find_by_npn).with(npn).and_return(broker)
+      end
+
+      it "should create the policy" do
+        expect(policy_factory).to receive(:create!).with(broker_create_params)
+        subject.execute(broker_request)
+      end
     end
   end
 end
