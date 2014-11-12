81de3fd637ffd3f28e24a9bac748895e07e88769
Raghuram <raghuramg83@gmail.com>
2014-11-12 16:57:54 -0500

    Create person usecase added

--- app/request_models/update_person_request.rb
+++ app/request_models/update_person_request.rb
@@ -1,48 +1,65 @@
 class UpdatePersonRequest
-  def from_cv(payload)
+  def from_cv(payload = nil)
+    # payload = File.open(Rails.root.to_s + "/individual_test.xml")
     parser = Nokogiri::XML(payload)
-    individual = Parsers::Xml::Reports::individual.new(parser.root)
+    individual = Parsers::Xml::Reports::Individual.new(parser.root)
+    @mapping = Parsers::Xml::Reports::GlueMappings.new
 
     request = {
-      person_id: individual.id,
-      member_id: individual.person.id,
-      names: person_names(individual), 
-      is_active: individual.is_active,
-      members: [member_details(individual)], 
-      addresses: individual.person.addresses.map{|address| address.marshal_dump},
-      emails: individual.person.emails.map{|email| email.marshal_dump},
-      relationships: individual.relationships.map{|relationship| relationship.marshal_dump},
-      job_title: individual.person.job_title,
-      department: individual.person.department,
-      is_active: individual.root_elements.is_active
+      member_id: strip_uri(individual.member_ids.id),
+      person: serialize_person(individual),
+      demographics: serialize_demographics(individual)
     }
-
+        
     request
   end
 
-  def person_names(individual)
-    {
-      name_pfx: individual.person.person_name.person_name_prefix_text,
-      name_first: individual.person.person_name.person_given_name,
-      name_middle: individual.person.person_name.person_middle_name,
-      name_last: individual.person.person_name.person_surname,
-      name_sfx: individual.person.person_name.person_name_suffix_text,
-      name_full: individual.person.person_name.person_full_name,
-      alternate_name: individual.person.person_name.person_alternate_name
+  def serialize_person(individual)
+    person_name = individual.person[:person_name]
+    person = {
+      name_pfx: person_name.person_name_prefix_text,
+      name_first: person_name.person_given_name,
+      name_middle: person_name.person_middle_name,
+      name_last: person_name.person_surname,
+      name_sfx: person_name.person_name_suffix_text,
+      name_full: person_name.person_full_name,
+      alternate_name: person_name.person_alternate_name
     }
+
+    person[:phones] = individual.person[:phones].map { |e| e.marshal_dump }
+    person[:addresses] = individual.person[:addresses].map { |e| map_with_glue(e.marshal_dump)  }
+    person[:emails] = individual.person[:emails].map { |e| e.marshal_dump }
+    person[:relationships] = individual.relationships.map { |e| e.marshal_dump  }
+
+    person[:id] = strip_uri(individual.person[:id].id)
+    person
   end
 
-  def member_details(individual)
+  def map_with_glue(properties)
+    glue_properties = {}
+    properties.each do |k, v|
+      glue_properties[@mapping.individual[k]] = v
+    end
+    glue_properties
+  end
+
+  def serialize_demographics(individual)
     {
       ssn: individual.demographics.ssn,
-      gender: individual.demographics.sex,
+      gender: strip_uri(individual.demographics.sex),
       dob: individual.demographics.birth_date,
       ethnicity: individual.demographics.ethnicity,
       race: individual.demographics.race,
       birth_location: individual.demographics.birth_location,
-      citizen_status: individual.demographics.citizen_status,
+      citizen_status: strip_uri(individual.demographics.citizen_status),
       is_state_resident: individual.demographics.is_state_resident,
       is_incarcerated: individual.demographics.is_incarcerated
     }
+  end
+
+  private
+
+  def strip_uri(text)
+    text.split('#')[1]
   end
 end
