d606608b5941f868fdf12ab7dbe146309eac9ec5
Raghuram <raghuramg83@gmail.com>
2014-11-13 12:39:34 -0500

    Added commit logic for Update Person usecase

--- app/request_models/update_person_request.rb
+++ app/request_models/update_person_request.rb
@@ -6,7 +6,7 @@ class UpdatePersonRequest
     @glue_mapping = Parsers::Xml::Reports::GlueMappings.new
 
     {
-      hbx_member_id: individual.member_ids.id,
+      hbx_member_id: individual.hbx_ids[:id],
       person: serialize_person(individual),
       demographics: map_with_glue(individual.demographics, @glue_mapping.demographics)
     }
@@ -14,16 +14,19 @@ class UpdatePersonRequest
 
   def serialize_person(individual)
     person = individual.person
-    person[:id] = individual.person[:id].id
+    person[:id] = individual.person[:id]
     person[:phones] = individual.person[:phones].map { |e| map_with_glue(e, @glue_mapping.phone) }
     person[:addresses] = individual.person[:addresses].map { |e| map_with_glue(e, @glue_mapping.address)  }
     person[:emails] = individual.person[:emails].map { |e| map_with_glue(e, @glue_mapping.email) }
     person
   end
 
+  private
+
   def map_with_glue(properties, mapping)
     properties.inject({}) do |data, (k, v)|
-      data[mapping[k]] = v
+      key = mapping.has_key?(k) ? mapping[k] : k
+      data[key] = v
       data
     end
   end
--- app/use_cases/update_person.rb
+++ app/use_cases/update_person.rb
@@ -55,13 +55,49 @@ class UpdatePerson
   def commit(request)
     person, member = person_and_member_match(request)
     if person
+      member_attributes = request[:demographics]
+      if member.nil?
+        member_attributes.merge({:hbx_member_id => request[:hbx_member_id]})
+      end
       member ||= person.members.new
-      member.attributes = request[:demographics]
-      member.save
-      # update person
-      # update emails
-      # update phones
-      # update addresses
+      member.attributes = member_attributes
+      member.save!
+
+      person_attributes = request[:person].inject({}) do |person, (k, v)|
+        person[k] = v if v.is_a?(String)
+        person
+      end
+      person.attributes = person_attributes
+      person.save!
+
+      request[:person][:addresses].each do |address|
+        if current_address = person.phones.detect{|x| x.address_type == phone[:address_type]}
+          current_address.update_attributes(address)
+        else
+          new_address = person.addresses.new(address)
+          new_address.save!
+        end
+      end
+
+      request[:person][:phones].each do |phone|
+        if current_phone = person.phones.detect{|x| x.phone_type == phone[:phone_type]}
+          current_phone.update_attributes(phone)
+        else
+          new_phone = person.phones.new(phone)
+          new_phone.save!
+        end
+      end
+
+      request[:person][:emails].each do |email|
+        if current_email = person.emails.detect{|x| x.email_type == email[:email_type]}
+          current_email.update_attributes(email)
+        else
+          new_email = person.emails.new(email)
+          new_email.save!
+        end
+      end
+
+      listener.register_person(request[:member], person, member)
     else
       @create_person_factory.commit(request)
     end
