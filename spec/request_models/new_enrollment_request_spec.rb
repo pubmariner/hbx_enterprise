1cebc30dc53e59b81a41ad5bb52310e7f2942ad5
Trey Evans <lewis.r.evans@gmail.com>
2014-11-14 16:53:59 -0500

    Updated spec.

--- spec/request_models/new_enrollment_request_spec.rb
+++ spec/request_models/new_enrollment_request_spec.rb
@@ -18,6 +18,22 @@ describe NewEnrollmentRequest do
     it "should have a single individual" do
       expect(subject[:individuals].length).to eql 1
     end
+
+    describe "then the single policy inside" do
+      let(:policy) { subject[:policies].first }
+
+      it "should have the correct enrollment group id" do
+        expect(policy[:enrollment_group_id]).to eql("-4601383594475126784")
+      end
+
+      it "should have the correct hios_id" do
+        expect(policy[:hios_id]).to eql("86052DC0410002-01")
+      end
+
+      it "should have the correct plan_year" do
+        expect(policy[:hios_id]).to eql("2014")
+      end
+    end
   end
 
 end
