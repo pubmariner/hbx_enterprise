8094002eafe0e43df8db79c92186a65681db331c
Raghuram <raghuramg83@gmail.com>
2014-11-14 10:41:28 -0500

    New Enrollment request added

--- app/request_models/new_enrollment_request.rb
+++ app/request_models/new_enrollment_request.rb
@@ -1,7 +1,13 @@
 class NewEnrollmentRequest
   def self.from_xml(payload)
+
     xml = Nokogiri::XML(payload)
 
-    {}
+    enrollment = Parsers::Xml::Reports::NewEnrollment.new(xml.root)
+
+    {
+      people: enrollment.people,
+      policies: enrollment.policies
+    }
   end
 end
