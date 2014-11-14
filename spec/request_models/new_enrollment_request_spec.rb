878afdce8d731858cb72d518c0675f5e0e96a2c6
Trey Evans <lewis.r.evans@gmail.com>
2014-11-14 16:48:56 -0500

    Updating request spec

--- spec/request_models/new_enrollment_request_spec.rb
+++ spec/request_models/new_enrollment_request_spec.rb
@@ -2,14 +2,22 @@ require "rails_helper"
 
 describe NewEnrollmentRequest do
 
-  let(:file_path) { File.join(Rails.root, "spec/data/request_models/new_enrollment_request/n4601383594475126784.xml") }
 
   let(:xml) { File.open(file_path).read }
 
   subject { NewEnrollmentRequest.from_xml(xml) }
 
-  it "should run" do
-    expect(subject).not_to be_nil
+  describe "with file n4601383594475126784.xml" do
+
+    let(:file_path) { File.join(Rails.root, "spec/data/request_models/new_enrollment_request/n4601383594475126784.xml") }
+
+    it "should have a single policy" do
+      expect(subject[:policies].length).to eql 1
+    end
+
+    it "should have a single individual" do
+      expect(subject[:individuals].length).to eql 1
+    end
   end
 
 end
