3c28f689b5dbfe45df229557abab06fc09c036ad
Trey Evans <lewis.r.evans@gmail.com>
2014-11-17 14:01:38 -0500

    Fixing routing.

--- app/models/listeners/qhp_selected_listener.rb
+++ app/models/listeners/qhp_selected_listener.rb
@@ -23,7 +23,7 @@ module Listeners
     # == Returns:
     #   The type of market :individual or :employee
     def market_type(event_name)
-      Maybe.new(event_name).split('#').first.split(":").last.to_sym.value
+      Maybe.new(event_name).split('#').first.split(":").last.value.contains?("employ") ? :employer_employee : :individual
     end
 
     # TODO: Parse out sep reason
