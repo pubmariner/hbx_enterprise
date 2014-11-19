4a1ad35c2f43a0efbc1b945994d1e3225bb88918
Trey Evans <lewis.r.evans@gmail.com>
2014-11-19 15:09:19 -0500

    Finished fix.

--- app/models/services/policy_publisher.rb
+++ app/models/services/policy_publisher.rb
@@ -1,13 +1,13 @@
 module Services
   class PolicyPublisher
     def self.publish(q_reason_uri, p_id)
-      reason = q_reason_uri.split("#").last
-      reason = (reason.downcase == "renewal") ? "renewal" : "initial_enrollment"
-      operation = (reason.downcase == "renewal") ? "change" : "add"
-      routing_key = (reason.downcase == "renewal") ? "policy.renewal" : "policy.initial_enrollment"
-      v_destination = (reason.downcase == "renewal") ? "hbx.maintenance_messages" : "hbx.enrollment_messages"
-
       policy = Policy.where(:id => p_id).first
+      p_action = policy_action(policy)
+      reason = (p_action.downcase == "renewal") ? "renewal" : "initial_enrollment"
+      operation = (p_action.downcase == "renewal") ? "change" : "add"
+      routing_key = (p_action.downcase == "renewal") ? "policy.renewal" : "policy.initial_enrollment"
+      v_destination = (p_action.downcase == "renewal") ? "hbx.maintenance_messages" : "hbx.enrollment_messages"
+
       xml_body = serialize(policy, operation, reason)
       with_channel do |channel|
         channel.direct(ExchangeInformation.request_exchange, :durable => true).publish(xml_body, {
@@ -40,6 +40,18 @@ module Services
       chan.prefetch(1)
       yield chan
       session.close
+    end
+
+    def self.policy_action(policy)
+      subscriber = policy.subscriber
+      coverage_start = subscriber.coverage_start
+      sub_person = subscriber.person
+      has_renewal_match = sub_person.policies.any? do |pol|
+        (pol.plan.coverage_type == policy.plan.coverage_type) &&
+          pol.active_as_of?(coverage_start - 1.day) &&
+          (pol.id != policy.id)
+      end
+      has_renewal_match ? "renewal" : "initial_enrollment"
     end
   end
 end
