501fc0b4477bb283eb2deb061ca69ecd2674bc1d
Trey Evans <lewis.r.evans@gmail.com>
2014-12-17 15:46:33 -0500

    Fixing some naming.

--- app/models/amqp/requestor.rb
+++ app/models/amqp/requestor.rb
@@ -3,29 +3,29 @@ require 'thread'
 
 module Amqp
   class Requestor
-    attr_reader :channel
-
     def initialize(chan)
       @channel = chan
     end
 
     def request(properties, payload, timeout = 15)
+      channel = @channel.connection.create_channel
       temp_queue = channel.queue("", :exclusive => true)
       request_exchange = channel.direct(ExchangeInformation.request_exchange, :durable => true)
-      request_exchange.publish(payload, properties.merge({ :reply_to => temp_queue.name, :persistent => true }))
-      delivery_info, properties, payload = [nil, nil, nil]
+      request_exchange.publish(payload, properties.dup.merge({ :reply_to => temp_queue.name, :persistent => true }))
+      delivery_info, r_props, r_payload = [nil, nil, nil]
       begin
         Timeout::timeout(timeout) do
           temp_queue.subscribe({:manual_ack => true, :block => true}) do |di, prop, pay|
-            delivery_info, properties, payload = [di, prop, pay]
+            delivery_info, r_props, r_payload = [di, prop, pay]
             channel.acknowledge(di.delivery_tag, false)
             throw :terminate, "success"
           end
         end
       ensure
         temp_queue.delete
+        channel.close
       end
-      [delivery_info, properties, payload]
+      [delivery_info, r_props, r_payload]
     end
 
     def self.default
