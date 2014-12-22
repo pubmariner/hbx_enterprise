905fb43b1b3689aed0e811395e84fdbd291c570b
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-22 14:30:21 -0500

    sending more params to .new() changed "" to nil test

--- spec/app/models/parsers/individual_cv_parser_spec.rb
+++ spec/app/models/parsers/individual_cv_parser_spec.rb
@@ -4,11 +4,10 @@ describe Parsers::IndividualCvParser do
 
   before(:all) do
     @xml = File.read("/Users/CitadelFirm/Downloads/projects/hbx/hbx_enterprise/spec/data/parsers/individual_cv.xml")
-    @individual_cv_parser = Parsers::IndividualCvParser.new(@xml)
+    @individual_cv_parser = Parsers::IndividualCvParser.new(@xml, nil, nil)
   end
 
   it 'initializes successfully' do
-
     expect(@individual_cv_parser.class).to eq(Parsers::IndividualCvParser)
   end
 
@@ -21,7 +20,6 @@ describe Parsers::IndividualCvParser do
   end
 
   it 'parses person demographic element' do
-
     expect(@individual_cv_parser.parser.person_demographics.ssn.nil?).to eq(false)
     expect(@individual_cv_parser.parser.person_demographics.sex.nil?).to eq(false)
     expect(@individual_cv_parser.parser.person_demographics.birth_date.nil?).to eq(false)
@@ -31,6 +29,28 @@ describe Parsers::IndividualCvParser do
     expect(@individual_cv_parser.parser.person_demographics.citizen_status.nil?).to eq(false)
     expect(@individual_cv_parser.parser.person_demographics.is_state_resident.nil?).to eq(true)
     expect(@individual_cv_parser.parser.person_demographics.is_incarcerated.nil?).to eq(true)
+  end
+
+  it "parses addresses correctly" do
+    expect(@individual_cv_parser.address[:address_line_1]).to eq("212 Nice Avenue NW")
+    expect(@individual_cv_parser.address[:address_line_2]).to eq("")
+    expect(@individual_cv_parser.address[:city]).to eq("Washington")
+    expect(@individual_cv_parser.address[:state]).to eq("DC")
+    expect(@individual_cv_parser.address[:zip]).to eq("20001")
+  end
+
+  it "parses phone number correctly" do
+
+    expect(@individual_cv_parser.phone[:country_code]).to be_nil
+    expect(@individual_cv_parser.phone[:area_code]).to be_nil
+    expect(@individual_cv_parser.phone[:extension]).to be_nil
+    expect(@individual_cv_parser.phone[:phone_number]).to be_nil
+    expect(@individual_cv_parser.phone[:full_phone_number]).to eq("919-919-9199225652")
+    expect(@individual_cv_parser.phone[:is_preferred]).to eq("true")
+    expect(@individual_cv_parser.phone[:type]).to eq("urn:openhbx:terms:v1:phone_type#home")
+  end
+
+  it "returns email" do
 
   end
 end
