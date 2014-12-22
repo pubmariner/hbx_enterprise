386909227c77380244093ad43ce24f91722ae533
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-22 10:22:47 -0500

    added expectations

--- spec/app/models/parsers/individual_cv_parser_spec.rb
+++ spec/app/models/parsers/individual_cv_parser_spec.rb
@@ -4,15 +4,33 @@ describe Parsers::IndividualCvParser do
 
   before(:all) do
     @xml = File.read("/Users/CitadelFirm/Downloads/projects/hbx/hbx_enterprise/spec/data/parsers/individual_cv.xml")
+    @individual_cv_parser = Parsers::IndividualCvParser.new(@xml)
   end
 
   it 'initializes successfully' do
-    individual_cv_parser = Parsers::IndividualCvParser.new(@xml)
-    expect(individual_cv_parser.class).to eq(Parsers::IndividualCvParser)
+
+    expect(@individual_cv_parser.class).to eq(Parsers::IndividualCvParser)
   end
 
   it 'supports parse method' do
-    individual_cv_parser = Parsers::IndividualCvParser.new(@xml)
-    expect(individual_cv_parser.parser.nil?).to eq(false)
+    expect(@individual_cv_parser.parser.nil?).to eq(false)
+  end
+
+  it 'parses person element' do
+    expect(@individual_cv_parser.parser.person.nil?).to eq(false)
+  end
+
+  it 'parses person demographic element' do
+
+    expect(@individual_cv_parser.parser.person_demographics.ssn.nil?).to eq(false)
+    expect(@individual_cv_parser.parser.person_demographics.sex.nil?).to eq(false)
+    expect(@individual_cv_parser.parser.person_demographics.birth_date.nil?).to eq(false)
+    expect(@individual_cv_parser.parser.person_demographics.death_date.nil?).to eq(true)
+    expect(@individual_cv_parser.parser.person_demographics.ethnicity.nil?).to eq(true)
+    expect(@individual_cv_parser.parser.person_demographics.marital_status.nil?).to eq(true)
+    expect(@individual_cv_parser.parser.person_demographics.citizen_status.nil?).to eq(false)
+    expect(@individual_cv_parser.parser.person_demographics.is_state_resident.nil?).to eq(true)
+    expect(@individual_cv_parser.parser.person_demographics.is_incarcerated.nil?).to eq(true)
+
   end
 end
