35fba1dc2c3946e328dffc3deef40b9650618161
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-30 12:14:36 -0500

    added enrollments controller. added EnrollmentVerifier to validate CV.

---
+++ lib/enrollment_verifier.rb
@@ -0,0 +1,11 @@
+class EnrollmentVerifier
+  #TODO to be implemented
+  def self.is_valid_xml?(xml)
+    true
+  end
+
+  #TODO to be implemented
+  def self.is_root_valid?(xml)
+    true
+  end
+end
