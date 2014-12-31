f123e07a09c5ea0a499556d58aeb9dcbce3bf6bd
Sagar Arlekar <sagar.arlekar@gmail.com>
2014-12-31 10:38:53 -0500

    test xml against xsd

--- lib/enrollment_verifier.rb
+++ lib/enrollment_verifier.rb
@@ -1,11 +1,29 @@
-class EnrollmentVerifier
-  #TODO to be implemented
-  def self.is_valid_xml?(xml)
-    true
-  end
+    class EnrollmentVerifier
 
-  #TODO to be implemented
-  def self.is_root_valid?(xml)
-    true
-  end
-end
+      POLICY_SCHEMA = File.join(PADRINO_ROOT, 'cv', 'policy.xsd')
+
+      def self.is_valid_xml?(xml)
+        begin
+          xsd = Nokogiri::XML::Schema(File.read(POLICY_SCHEMA))
+          doc = Nokogiri::XML(xml)
+
+          xsd.validate(doc).each do |error|
+            puts error.message
+          end
+
+          if xsd.validate(doc).size == 0 #the number of errors
+            return true
+          else
+            return false
+          end
+        rescue Exception=>e
+          puts e.message
+          false
+        end
+      end
+
+      #TODO to be implemented
+      def self.is_root_valid?(xml)
+        true
+      end
+    end
