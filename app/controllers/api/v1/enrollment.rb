30c768241d842983c51256a29e9b724533bfd59c
Trey Evans <lewis.r.evans@gmail.com>
2015-01-02 12:43:54 -0500

    Fixing return status.

--- app/controllers/api/v1/enrollment.rb
+++ app/controllers/api/v1/enrollment.rb
@@ -16,7 +16,7 @@ HbxEnterprise::App.controllers :enrollments, map: '/api/v1/enrollment' do
       status 202
       body "<response><success>Success. XML accepted.</success></response>"
     else
-      halt(422, "<response><failure>#{enrollment_validator.errors.full_messages}</failure></response>")
+      halt(422, enrollment_validator.errors.to_xml)
     end
   end
 end
