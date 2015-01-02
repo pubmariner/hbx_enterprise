c14b9a39ee8cfa13d9e77b3067b92b59538b0a61
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-01-02 12:35:26 -0500

    422 now has a xml error response

--- app/controllers/api/v1/enrollment.rb
+++ app/controllers/api/v1/enrollment.rb
@@ -16,7 +16,7 @@ HbxEnterprise::App.controllers :enrollments, map: '/api/v1/enrollment' do
       response.status = 200
       body "<response><success>Success. XML accepted.</success></response>"
     else
-      halt(422, enrollment_validator.errors.full_messages)
+      halt(422, "<response><failure>#{enrollment_validator.errors.full_messages}</failure></response>")
     end
   end
 end
