ca9f0783f2407061c56ebc75e60370d44d198da5
Trey Evans <lewis.r.evans@gmail.com>
2015-01-15 17:17:05 -0500

    Adding csv output params.

--- app/models/listeners/employer_digest_listener.rb
+++ app/models/listeners/employer_digest_listener.rb
@@ -1,5 +1,14 @@
+require 'csv'
+
 module Listeners
   class EmployerDigestListener < Amqp::Client
+    OUTPUT_FILE_NAME = "digest_file.csv"
+
+    def initialize(ch, q, csv)
+      super(ch,q)
+      @csv = csv
+    end
+
     def on_message(delivery_info, properties, payload)
       failed = is_error?(delivery_info)
       # Only the error case will always have these result status and error_code
@@ -33,9 +42,11 @@ module Listeners
       ch.prefetch(1)
       dex = ch.default_exchange
       q = ch.queue(queue_name, :durable => true)
-      client = self.new(ch, q)
-      return if client.no_messages_remaining?
-      client.subscribe(:block => true, :manual_ack => true)
+      CSV.open(OUTPUT_FILE_NAME) do |csv|
+        client = self.new(ch, q, csv)
+        return if client.no_messages_remaining?
+        client.subscribe(:block => true, :manual_ack => true)
+      end
     end
   end
 end
