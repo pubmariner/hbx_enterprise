b5b743511e906b3199fa2faf00a3b93014b1ed82
Raghuram <raghuramg83@gmail.com>
2015-01-15 14:22:49 -0500

    update for enrollee spec

--- spec/app/models/parsers/enrollee_parser_spec.rb
+++ spec/app/models/parsers/enrollee_parser_spec.rb
@@ -8,20 +8,26 @@ describe Parsers::Xml::Cv::EnrolleeParser do
 
   let(:begin_date) { "20150101" }
   let(:premium_amount) { "275.05" }
+  let(:premium_amount1) { "247.43" }
 
   subject {
     Parsers::Xml::Cv::EnrollmentParser.parse(enrollment, :single => true)
   }
 
-  let(:enrollee) { subject.enrollees.first }
+  let(:enrollee) { subject.enrollees[0] }
+  let(:enrollee1) { subject.enrollees[1] }
 
   it 'should return member' do 
     expect(enrollee).to_not be_nil
     expect(enrollee.member).to be_a_kind_of(Parsers::Xml::Cv::IndividualParser)
+    expect(enrollee).to_not be_nil
+    expect(enrollee1.member).to be_a_kind_of(Parsers::Xml::Cv::IndividualParser)
   end
 
   it 'should have begin date and premium amount' do
     expect(enrollee.begin_date).to eq(begin_date)
     expect(enrollee.premium_amount).to eq(premium_amount)
+    expect(enrollee1.begin_date).to eq(begin_date)
+    expect(enrollee1.premium_amount).to eq(premium_amount1)
   end
 end
