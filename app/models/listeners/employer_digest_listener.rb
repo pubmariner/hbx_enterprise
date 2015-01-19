13feb24547a1c5a1f35d28312a9e0d8c3740e595
Trey Evans <lewis.r.evans@gmail.com>
2015-01-19 13:34:26 -0500

    Updating deployment buckets.

--- app/models/listeners/employer_digest_listener.rb
+++ app/models/listeners/employer_digest_listener.rb
@@ -3,6 +3,7 @@ require 'csv'
 module Listeners
   class EmployerDigestListener < Amqp::Client
     OUTPUT_FILE_NAME = "digest_file.csv"
+    DIGEST_TYPES = ["individual", "employer_employee"]
 
     def initialize(ch, q, csv)
       super(ch,q)
@@ -30,17 +31,24 @@ module Listeners
       delivery_info.routing_key.split(".").first == "error"
     end
 
-    def self.queue_name
-      ExchangeInformation.queue_name_for(self)
+    def self.queue_name_for(digest_type)
+      ec = ExchangeInformation
+      "#{ec.hbx_id}.#{ec.environment}.q.hbx_enterprise.enrollment_digest_#{digest_type}"
     end
 
-    def self.run
+    def self.event_keys_for(digest_type)
+      ["info.events.#{digest_type}.initial_enrollment",
+      "error.events.#{digest_type}.initial_enrollment"]
+    end
+
+    def self.run(digest_type)
+      raise ArgumentError.new("Digest type must be: individual or employer_employee") unless DIGEST_TYPES.include?(digest_type)
       conn = Bunny.new(ExchangeInformation.amqp_uri)
       conn.start
       ch = conn.create_channel
       ch.prefetch(1)
       dex = ch.default_exchange
-      q = ch.queue(queue_name, :durable => true)
+      q = ch.queue(queue_name_for(digest_type), :durable => true)
       trap("SIGINT") { throw :terminate }
       ManualEnrollments::EnrollmentDigest.with_csv_template do |csv|
         client = self.new(ch, q, csv)
