88f7c9908d766c29d059bbb430fd7541659ebbb6
Trey Evans <lewis.r.evans@gmail.com>
2015-01-29 10:21:46 -0500

    Try to create script.

---
+++ amqp/download_shops.rb
@@ -0,0 +1,51 @@
+class SaveShopOrdered
+
+  def self.fill_pipe
+    r, w = IO.pipe
+    @@pipe_r = r
+    @@pipe_w = w
+    dir_glob = File.open("amqp/prod_errors/missing_enrolls.txt").read.split("\n")
+    dir_glob.each do |f|
+      @@pipe_w.puts(f)
+    end
+  end
+
+  def self.run
+    @@pipe_w.close
+    conn = Bunny.new(ExchangeInformation.amqp_uri)
+    conn.start
+    ch = conn.create_channel
+    rch = conn.create_channel
+    dep = Listeners::DcasEnrollmentProvider.new(ch, nil, ch.default_exchange)
+    # dir_glob = File.open("amqp/prod_errors/missing_enrolls.txt").read.split("\n")
+    while(l = @@pipe_r.gets)
+      begin
+        ts_string,f = l.strip.split("_")
+#        ts_string = Time.now.strftime("%Y%m%d%H%M%S")
+        enrollment_props = {
+          :headers => {
+            "enrollment_group_id" => f,
+            "submitted_timestamp" => ts_string
+          }
+        }
+        retrieve_demographics = Services::RetrieveDemographics.new(f)
+        response_cv = dep.convert_to_cv(OpenStruct.new(enrollment_props), retrieve_demographics)
+        file_name = ts_string + "_" + f
+        f_open = File.open(
+          File.join(File.dirname(__FILE__), "switch_cvs", "#{file_name}.xml"),
+          "w"
+        )
+        f_open.puts(response_cv)
+        f_open.close
+      rescue => e
+        puts f 
+        puts e.inspect
+        #        puts e.backtrace[0..30].join("\n")
+      end
+    end
+  end
+end
+
+SaveShopOrdered.fill_pipe
+# SaveShopOrdered.run
+Forkr.new(SaveShopOrdered, 10).run
---
+++ amqp/try_to_create.rb
@@ -0,0 +1,42 @@
+class TryCreate
+
+  def self.run
+    conn = Bunny.new(ExchangeInformation.amqp_uri)
+    conn.start
+    ch = conn.create_channel
+    rk = "dc0.preprod.glue.enrollment_creator"
+    dir_glob = Dir.glob(File.join(File.dirname(__FILE__), "switch_cvs", "*.xml"))
+    out_file_base = File.join(File.dirname(__FILE__), "switch_fails")
+    dir_glob.each do |f|
+      begin
+      en_data = File.open(f, 'r') do |tf|
+        tf.read
+      end
+      request_props = {
+        :routing_key => "enrollment.create",
+        :headers => {
+          :qualifying_reason_uri => "urn:dc0:terms:v1:qualifying_life_event#initial_enrollment"
+        }
+      }
+      req = Amqp::Requestor.default
+      di, r_props, r_body = req.request(request_props, en_data)
+      r_status = r_props.headers["return_status"]
+      case r_status
+      when "200"
+      else
+        f_name = File.basename(f).gsub(".xml", ".json")
+        File.open(File.join(out_file_base, f_name), "w") do |o_file|
+          o_file.puts(r_body)
+        end
+      end
+
+      rescue
+        puts f
+        raise $!
+      end
+    end
+  end
+
+end
+
+TryCreate.run
