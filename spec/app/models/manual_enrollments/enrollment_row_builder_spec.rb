e3cea78dbf43cec694ee89516bded5c014440d9e
Raghuram <raghuramg83@gmail.com>
2015-04-16 12:30:20 -0400

    updates for enrollment row builder spec

--- spec/app/models/manual_enrollments/enrollment_row_builder_spec.rb
+++ spec/app/models/manual_enrollments/enrollment_row_builder_spec.rb
@@ -6,7 +6,7 @@ module ManualEnrollments
     subject { EnrollmentRowBuilder.new }
 
     context '#sort_enrollees_by_rel' do
-      
+
       let(:spouse) { double(member: double(relationship_uri: "urn:openhbx:terms:v1:individual_relationship#Spouse")) }
       let(:child) { double(member: double(relationship_uri: "urn:openhbx:terms:v1:individual_relationship#Child")) }
       let(:son) { double(member: double(relationship_uri: "urn:openhbx:terms:v1:individual_relationship#Son")) }
@@ -14,24 +14,31 @@ module ManualEnrollments
       let(:subscriber) { double(member: double(relationship_uri: "urn:openhbx:terms:v1:individual_relationship#Self")) }
       let(:no_rel) { double(member: double(relationship_uri: nil)) }
 
-      context 'when dependent, spouse not ordered properly' do
+      context 'when dependent, spouse not sorted properly' do
         let(:enrollees) { [spouse, child, subscriber] }
         it 'should return them in correct order with subscriber first' do
           expect(subject.sort_enrollees_by_rel(enrollees)).to eq([subscriber, spouse, child])
         end
       end
 
-      context 'when relationship not one of child, spouse, self' do
+      context 'when relationship is not child, spouse, self' do
         let(:enrollees) { [child, son, spouse, dependent, subscriber] }
-        it 'should return enrollees in correct order by putting unknown rel at the end' do
+        it 'should sort enrollees by putting unknown relationships at the end' do
           expect(subject.sort_enrollees_by_rel(enrollees)).to eq([subscriber, spouse, child, son, dependent])
         end
       end
 
       context 'when relationship empty' do
         let(:enrollees) { [son, no_rel, child, spouse, subscriber] }
-        it 'should return enrollees in correct order by putting empty at the end' do
+        it 'should sort enrollees by putting empty relationship at the end' do
           expect(subject.sort_enrollees_by_rel(enrollees)).to eq([subscriber, spouse, child, son, no_rel])
+        end
+      end
+
+      context 'when enrollees in the correct order' do 
+        let(:enrollees) { [subscriber, spouse, child]}
+        it 'should return them as is' do 
+          expect(subject.sort_enrollees_by_rel(enrollees)).to eq(enrollees)
         end
       end
     end
