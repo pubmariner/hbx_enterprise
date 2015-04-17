f6824c16e035d3a0fe117ed53431871781229506
Trey Evans <lewis.r.evans@gmail.com>
2015-04-17 10:53:18 -0400

    Simple interface for transformation.

---
+++ app/models/transformers/csv_to_xml_transformer.rb
@@ -0,0 +1,16 @@
+module Transformers
+  class CsvToXmlTransformer
+
+    def initialize(row)
+
+    end
+
+    def transform
+
+    end
+
+    def valid?
+
+    end
+  end
+end
---
+++ app/models/transformers/xml_to_csv_transformer.rb
@@ -0,0 +1,12 @@
+module Transformers
+  class XmlToCsvTransformer
+    def initialize(cv)
+
+    end
+
+    # returns 1 row for each policy
+    def transform
+
+    end
+  end
+end
