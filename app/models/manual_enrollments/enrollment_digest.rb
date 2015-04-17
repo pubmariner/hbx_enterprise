a801b4086886ae208403549d6b0666d6e1815c60
Raghuram <raghuramg83@gmail.com>
2015-04-17 12:14:05 -0400

    Added xml to csv interface

--- app/models/manual_enrollments/enrollment_digest.rb
+++ app/models/manual_enrollments/enrollment_digest.rb
@@ -6,28 +6,12 @@ module ManualEnrollments
     def self.build_csv(payload, is_shop)
       # payload = File.read("#{Padrino.root.to_s}/spec/data/parsers/shop_enrollment.xml")
       enrollment = Parsers::Xml::Cv::EnrollmentParser.parse(payload)
-      hbx_enrollment = enrollment.policy.hbx_enrollment
-
-      if hbx_enrollment.blank?
+      if enrollment.policy.hbx_enrollment.blank?
         raise "Missing enrollment details"
       end
 
-      builder = EnrollmentRowBuilder.new
-      builder.append_enrollment_type
-      builder.append_market(is_shop)
-      builder.append_employer(hbx_enrollment)
-      builder.append_broker(enrollment.policy.broker)
-      builder.append_begin_date(enrollment.policy)
-      builder.append_plan_name(hbx_enrollment.plan)
-      builder.append_qhp_id
-      builder.append_csr_info
-      builder.append_csr_varient
-      builder.append_plan_hios(hbx_enrollment.plan)
-      builder.append_premium(hbx_enrollment)
-      builder.append_aptc(hbx_enrollment)
-      builder.append_responsible_amount(hbx_enrollment)
-      builder.append_enrollees(enrollment.policy)
-      builder.data_set
+      builder = EnrollmentRowBuilder.new(enrollment, is_shop)
+      builder.to_csv
     end
 
     def self.with_csv_template
@@ -39,6 +23,5 @@ module ManualEnrollments
         yield csv
       end
     end
-
   end
 end
