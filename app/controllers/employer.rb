6d5886b8098b5c7eeac714bfa67f6267bd7ee2ef
Sagar Arlekar <sagar.arlekar@gmail.com>
2015-12-09 16:21:07 -0500

    Refs #2571 haml and parsers for employer

--- app/controllers/employer.rb
+++ app/controllers/employer.rb
@@ -1,7 +1,16 @@
 class HbxEnterprise::App
   post '/employers/legacy_xml' do
 
+    file_path = "/Users/CitadelFirm/Downloads/cvs_dec_8/renewal/"
+
     @cv_hash = Parsers::Xml::Cv::OrganizationParser.parse(request.body.read).to_hash
-    render "employers/legacy_employer"
+
+    @carriers = @cv_hash[:employer_profile][:plan_years].first[:elected_plans].map do |plan| plan[:carrier][:name] end.uniq
+
+    @carriers.each do |carrier|
+      @carrier = carrier
+      group_xml = render "employers/legacy_employer"
+      File.open(file_path + @carrier + ".xml", 'a') { |file| file.write(group_xml) }
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,20 @@ HbxEnterprise::App.helpers do
     original_date = Date.strptime(date, "%Y%m%d")
     original_date.strftime("%Y-%m-%d")
   end
+
+  def primary_office_location(office_locations)
+    office_locations.detect do |office_location|
+      office_location[:primary] == 'true'
+    end
+  end
+
+  def plan_year_for_year(plan_years, year)
+    plan_years.detect do |plan_year|
+      Date.strptime(plan_year[:plan_year_start], "%Y%m%d").year.to_s == year
+    end
+  end
+
+  def plan_exchange_id(hios_id, active_year)
+    active_year + "" + hios_id.gsub(/[^0-9]/, '')
+  end
 end
