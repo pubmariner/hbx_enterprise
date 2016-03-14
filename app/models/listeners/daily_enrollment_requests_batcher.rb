404f977ce3346d35347a40d790b46192d8800c58
Trey Evans <lewis.r.evans@gmail.com>
2016-03-14 18:00:56 -0400

    Adding outstanding policy request batcher.

---
+++ app/models/listeners/daily_enrollment_requests_batcher.rb
@@ -0,0 +1,57 @@
+module Listeners
+  class DailyEnrollmentStatusBatcher < Amqp::Client
+    def on_message(delivery_info, properties, payload)
+      requestor = ::Amqp::Requestor.new(connection)
+      existing_ids_properties = {
+        :routing_key => "glue.policy_id_list"
+      }
+      eid_di, eid_props, eid_payload = requestor.request(
+        existing_ids_properties,
+        ""
+      )
+      query_request_props = {
+        :routing_key => "enroll.policy_query"
+      }
+      qr_di, qr_props, qr_payload = requestor.request(
+        query_request_props,
+        eid_payload
+      )
+      p_id_list = JSON.load(qr_payload)
+      batch_size = p_id_list.length
+      new_batch_name = OpenhbxWorkflow::Coordination.generate_batch_name
+      with_confirmed_channel do |chan|
+        out_ex = chan.fanout(ExchangeInformation.event_publish_exchange, :durable => true)
+        p_id_list.each_with_index do |p_id, idx|
+          out_ex.publish("", {
+            :routing_key => "info.events.policy.outstanding_policy_batch_item",
+            :timestamp => Time.now.to_i,
+            :app_id => "hbx_enterprise",
+            :headers => {
+              :batch_name => new_batch_name.to_s,
+              :index => idx,
+              :batch_size => batch_size,
+              :policy_id => p_id
+            }
+          })
+        end
+      end
+      channel.ack(delivery_info.delivery_tag,false)
+    end
+
+    def self.queue_name
+      ec = ExchangeInformation
+      "#{ec.hbx_id}.#{ec.environment}.q.hbx_enterprise.outstanding_enrollment_batcher"
+    end
+
+    def self.run
+      conn = Bunny.new(ExchangeInformation.amqp_uri, :heartbeat => 15)
+      conn.start
+      ch = conn.create_channel
+      ch.prefetch(1)
+      q = ch.queue(queue_name, :durable => true)
+
+      self.new(ch, q).subscribe(:block => true, :manual_ack => true)
+      conn.close
+    end
+  end
+end
