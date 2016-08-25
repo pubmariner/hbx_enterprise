f2e8440f451e73437e702254b0571df7e87304a5
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-08-25 16:56:20 -0400

    changed script to run broker xmls

--- script/generate_broker_xmls.rb
+++ script/generate_broker_xmls.rb
@@ -2,11 +2,11 @@ require 'net/http'
 require 'pry'
 
 
-dir_path = "" # Location of CVs. e.g. /Users/Downloads/brokers_with_office_locations/
-url_string = "" # REST URL where the project is running e.g. http://localhost:3004/brokers/legacy_xml
+dir_path = "/Users/CitadelFirm/Downloads/broker-xmls-aug24/10527-cvs/" # Location of CVs. e.g. /Users/Downloads/brokers_with_office_locations/
+url_string = "http://localhost:3004/brokers/legacy_xml" # REST URL where the project is running e.g. http://localhost:3004/brokers/legacy_xml
 
 def post_xml url_string, xml_string
-  uri = URI.parse url_string
+  uri = URI.parse URI.encode url_string
   request = Net::HTTP::Post.new uri.path
   request.body = xml_string
   request.content_type = 'text/xml'
@@ -14,7 +14,9 @@ def post_xml url_string, xml_string
   response.body
 end
 
+count = 0
 Dir.glob("#{dir_path}/**/*").each do |file_path|
+  count += 1
   xml_string = File.read(file_path)
   post_xml url_string, xml_string
 end
