ddc0c7575c7af391b8bea81e1ebcd11bd6a441f2
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-08-25 16:52:37 -0400

    changed broker + employer controller actions

--- app/controllers/broker.rb
+++ app/controllers/broker.rb
@@ -1,10 +1,9 @@
 class HbxEnterprise::App
   post '/brokers/legacy_xml' do
     @cv_hash = Parsers::Xml::Cv::IndividualParser.parse(request.body.read).to_hash
-
-    file_path = ""
+    file_path = "/Users/CitadelFirm/Downloads/broker-xmls-aug24/10527-cvs/broker.xml"
     broker_xml = render "brokers/legacy_broker", :locals =>{ :individual=> @cv_hash }
-    File.open(file_path, 'a') { |file| file.write(broker_xml) }
-    render nothing: true
+    File.open(file_path, 'a') { |file| file.write(broker_xml); }
+    return
   end
 end
--- app/controllers/employer.rb
+++ app/controllers/employer.rb
@@ -3,12 +3,17 @@ class HbxEnterprise::App
 
     @cv_hash = Parsers::Xml::Cv::OrganizationParser.parse(request.body.read).to_hash
 
-    #@plan_year = plan_year_for_year(@cv_hash[:employer_profile][:plan_years], '2016', '01', '04')
-    @plan_year = latest_plan_year(@cv_hash[:employer_profile][:plan_years])
+    @plan_year = plan_year_for_year(@cv_hash[:employer_profile][:plan_years], '2015', '01', '08')
+    #@plan_year = latest_plan_year(@cv_hash[:employer_profile][:plan_years])
+
+    if @plan_year.nil?
+      puts "Plan Year is nil FEIN #{@cv_hash[:fein]}"
+      return
+    end
 
     @carriers = @plan_year[:elected_plans].map do |plan| plan[:carrier][:name] end.uniq
 
-    dir_path = ""
+    dir_path = "/Users/CitadelFirm/Downloads/cvs-aug22/8038/"
 
     @carriers.each do |carrier|
       @carrier = carrier
