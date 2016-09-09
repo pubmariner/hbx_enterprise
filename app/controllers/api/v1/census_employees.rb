1cdb444b8cb9b2c304b3b301b2c59d3cebea5f51
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-09-09 16:11:53 -0400

    Refs #8033 parsing + setting headers

--- app/controllers/api/v1/census_employees.rb
+++ app/controllers/api/v1/census_employees.rb
@@ -6,7 +6,6 @@ HbxEnterprise::App.controllers :enrollments, map: '/api/v1' do
     request_xml = request.body.read
 
     delivery_info, properties, payload = process_request(request_xml)
-
     status properties[:headers][:return_status]
     body payload
   end
@@ -16,9 +15,13 @@ HbxEnterprise::App.controllers :enrollments, map: '/api/v1' do
     conn = Bunny.new(ExchangeInformation.amqp_uri, :heartbeat => 5)
     conn.start
 
+    request_hash = Parsers::Xml::Cv::EmployerRequestParser.parse(request_xml)
+    ssn = request_hash[:request][:parameters][:ssn]
+    dob = request_hash[:request][:parameters][:dob]
+
     request_properties = {
         :routing_key => "#{ExchangeInformation.hbx_id}.#{ExchangeInformation.environment}.q.enroll.census_employee_listener",
-        :headers => {}
+        :headers => { ssn:ssn, dob:dob }
     }
 
     Amqp::Requestor.new(conn).request(request_properties, request_xml, 180) #returns [delivery_info, properties, payload]
