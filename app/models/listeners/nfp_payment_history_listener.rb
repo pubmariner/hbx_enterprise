f586ee983c782704660342ad09eb28ccb84d578c
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-05-08 12:53:46 -0400

    Refs #14779 added 4 listeners for each NFP request

---
+++ app/models/listeners/nfp_payment_history_listener.rb
@@ -0,0 +1,35 @@
+module Listeners
+  class NfpPaymentHistoryListener < Amqp::Client
+    def get_service
+      ::Proxies::NfpSoapRequest.new
+    end
+
+    def service_failure_tag
+      "nfp.payment_history_response"
+    end
+
+    def self.queue_name
+      ec = ExchangeInformation
+      "#{ec.hbx_id}.#{ec.environment}.q.hbx_enterprise.nfp_payment_history"
+    end
+
+    def response_key
+      "info.events.nfp.nfp_payment_history_response"
+    end
+
+    def routing_key
+      "info.events.nfp.nfp_payment_history_request"
+    end
+
+    def self.run
+      conn = Bunny.new(ExchangeInformation.amqp_uri, :heartbeat => 15)
+      conn.start
+      ch = conn.create_channel
+      ch.prefetch(1)
+      q = ch.queue(queue_name, :durable => true)
+
+      self.new(ch, q).subscribe(:block => true, :manual_ack => true)
+      conn.close
+    end
+  end
+end
---
+++ app/models/listeners/nfp_pdf_listener.rb
@@ -0,0 +1,35 @@
+module Listeners
+  class NfpPdfListener < Amqp::Client
+    def get_service
+      ::Proxies::NfpSoapRequest.new
+    end
+
+    def service_failure_tag
+      "nfp.pdf_response"
+    end
+
+    def self.queue_name
+      ec = ExchangeInformation
+      "#{ec.hbx_id}.#{ec.environment}.q.hbx_enterprise.nfp_pdf"
+    end
+
+    def response_key
+      "info.events.nfp.nfp_pdf_response"
+    end
+
+    def routing_key
+      "info.events.nfp.nfp_pdf_request"
+    end
+
+    def self.run
+      conn = Bunny.new(ExchangeInformation.amqp_uri, :heartbeat => 15)
+      conn.start
+      ch = conn.create_channel
+      ch.prefetch(1)
+      q = ch.queue(queue_name, :durable => true)
+
+      self.new(ch, q).subscribe(:block => true, :manual_ack => true)
+      conn.close
+    end
+  end
+end
