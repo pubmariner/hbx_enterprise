a0b973416126c6d714a11258f6e8d25b07f3a18b
Andrej Rasevic <andrej.petar.rasevic@gmail.com>
2017-05-16 15:43:52 -0400

    Refs #14779 added listeners for response and message methods to enroll

--- app/models/listeners/nfp_payment_history_listener.rb
+++ app/models/listeners/nfp_payment_history_listener.rb
@@ -1,5 +1,23 @@
 module Listeners
   class NfpPaymentHistoryListener < Amqp::Client
+
+    def on_message(delivery_info, properties, payload)
+      headers = (properties.headers || {})
+      code, body = Proxies::NfpSoapRequest.new.request(headers.stringify_keys, 10)
+      case code.to_s
+      when "201"
+        # ALL GOOD
+        send_response(code.to_s, headers, body)
+        channel.acknowledge(delivery_info.delivery_tag, false)
+      when "503"
+        log_failure("error.events.employer.nfp_payment_history_request_timeout",code, headers, body)
+        channel.nack(delivery_info.delivery_tag,false, true)
+      else
+        log_failure("error.events.employer.nfp_payment_history_request_failure",code, headers, body)
+        channel.acknowledge(delivery_info.delivery_tag, false)
+      end
+    end
+
     def get_service
       ::Proxies::NfpSoapRequest.new
     end
@@ -19,6 +37,34 @@ module Listeners
 
     def routing_key
       "info.events.employer.nfp_payment_history_request"
+    end
+
+    def log_failure(key, code, headers, body)
+      r_channel = connection.create_channel
+      ex = r_channel.fanout(ExchangeInformation.event_publish_exchange, {:durable => true})
+      response_properties = {
+        :timestamp => Time.now.to_i,
+        :routing_key => key,
+        :headers => headers.merge({
+          :return_status => code
+        })
+      }
+      ex.publish(body.to_s || "", response_properties)
+      r_channel.close
+    end
+
+    def send_response(status, headers, r_body)
+      r_channel = connection.create_channel
+      ex = r_channel.fanout(ExchangeInformation.event_publish_exchange, {:durable => true})
+      response_properties = {
+        :timestamp => Time.now.to_i,
+        :routing_key => "info.events.employer.nfp_payment_history_success",
+        :headers => headers.merge({
+          :return_status => status
+        })
+      }
+      ex.publish(r_body.to_s || "", response_properties)
+      r_channel.close
     end
 
     def self.run
