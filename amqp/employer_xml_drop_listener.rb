0f8304e003d577c5b4f99f2c5a855211c912ca0e
Trey Evans <lewis.r.evans@gmail.com>
2017-07-31 11:19:27 -0400

    Refs #17469.  Enable correct listeners.

--- amqp/employer_xml_drop_listener.rb
+++ amqp/employer_xml_drop_listener.rb
@@ -1,13 +1,13 @@
 require "multi_forkr"
 
 legacy_listeners = {
-  Listeners::EmployerLegacyDigestListener => 1,
-  Listeners::EmployerLegacyDigestTransformer => 1,
-  Listeners::EmployerDigestPaymentProcessorListener => 1
+  Listeners::EmployerLegacyDigestListener => 1
 }
 
 employer_drop_listener = {
-    Listeners::EmployerDigestDropListener => 1
+  Listeners::EmployerDigestDropListener => 1,
+  Listeners::EmployerLegacyDigestTransformer => 1,
+  Listeners::EmployerDigestPaymentProcessorListener => 1
 }
 
 employer_listeners = ExchangeInformation.provide_legacy_employer_group_files? ? employer_drop_listener.merge(legacy_listeners) : employer_drop_listener
