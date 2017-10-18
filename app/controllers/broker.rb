2cde2a0db9b5c4caf10dc4a8ba1a497e127852fe
saimekala07 <saimekala07@gmail.com>
2017-10-18 16:14:02 -0400

    refs #19080

--- app/controllers/broker.rb
+++ app/controllers/broker.rb
@@ -2,30 +2,26 @@ require 'nokogiri'
 
 class HbxEnterprise::App
 
-  XML_HEADER = <<-XMLCODE
-<?xml version='1.0' encoding='utf-8' ?>
-<brokers xmlns:ns1='urn:dchbx:brokerv1' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='urn:dchbx:brokerv1' xsi:schemaLocation='urn:dchbx:broker broker_20130903.xsd'>
-  XMLCODE
-
   post '/brokers/legacy_xml' do
 
     file_path = " " # dir_path where broker xml that need to be generated for NFP.
-    File.open(file_path, 'a') { |file| file.write(XML_HEADER); } unless File.exists?(file_path)
-
     carrier_br_file_path = " " # dir_path where broker xml that need to be generated for carrier's.
-    File.open(carrier_br_file_path, 'a') { |file| file.write(XML_HEADER); } unless File.exists?(carrier_br_file_path)
 
     @cv_hash = Parsers::Xml::Cv::IndividualParser.parse(request.body.read).to_hash
     broker_xml = render "brokers/legacy_broker", :locals =>{ :individual=> @cv_hash }
     File.open(file_path, 'a') { |file| file.write(broker_xml); }
-
     doc = Nokogiri::XML(broker_xml)
 
-    if doc.xpath('//*[name()="ns1:broker_payment_accounts"]').present?  # removing broker_payment_accounts, broker ACH info should not be include in carrier's broker xml.
+    if doc.xpath("//cv:broker_payment_accounts", {:cv => 'urn:dchbx:brokerv1'}).present? # removing broker_payment_accounts, broker ACH info should not be include in carrier's broker xml.
 
-      doc.xpath('//*[name()="ns1:broker_payment_accounts"]').each do |node|
+      doc.xpath("//cv:broker_payment_accounts/cv:broker_payment_account", {:cv => 'urn:dchbx:brokerv1'}).each do |node|
         node.remove
       end
+      
+      doc.xpath("//cv:broker_payment_accounts", {:cv => 'urn:dchbx:brokerv1'}).each do |node|
+        node.remove
+      end
+
       File.open(carrier_br_file_path, 'a') { |file| file.write(doc.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::NO_DECLARATION, :indent => 2)); }
     else
       File.open(carrier_br_file_path, 'a') { |file| file.write(doc.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::NO_DECLARATION, :indent => 2)); }
